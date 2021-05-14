1c55e4b06bb78d34bab457001fd0862d76d037c2
Max Moeschinger <m.moeschinger@gmail.com>
2021-05-14 12:08:42 +0200

    update health endpoint

--- sourcecode/apis/resources/src/routes/index.js
+++ sourcecode/apis/resources/src/routes/index.js
@@ -83,7 +83,7 @@ export default async () => {
         runAsync(resourceController.ensureResourceExists)
     );
 
-    router.get('/resources/_ah/health', (req, res) => {
+    router.get('/_ah/health', (req, res) => {
         const probe = req.query.probe;
 
         if (probe === 'readiness') {
