da25c8a20a96222d3ffaa6afaf8fe2e67920322b
Brett Kromkamp <brett.kromkamp@cerpus.com>
2021-05-28 14:20:20 +0200

    Add initial SSG setup (for docs.edlib.com)

---
+++ sourcecode/docs/docs.edlib.com/docusaurus.config.js
@@ -0,0 +1,149 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Edlib',
+  tagline: 'Edlib allows you to create, store, share and use rich, interactive learning resources in the cloud',
+  url: 'https://docs.edlib.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'cerpus', // Usually your GitHub org/user name.
+  projectName: 'Edlib', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Edlib',
+      logo: {
+        alt: 'Edlib Logo',
+        src: 'img/edlib-logo.png',
+      },
+      items: [
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/cerpus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Product',
+          items: [
+            {
+              label: 'Features',
+              to: '/docs/product/features',
+            },
+            {
+              label: 'Roadmap',
+              to: '/docs/product/roadmap',
+            },
+            {
+              label: 'User guides',
+              to: '/docs/product/user-guides',
+            },
+            {
+              label: 'Demos',
+              to: '/docs/product/demos',
+            },
+            {
+              label: 'Knowledge base',
+              to: '/docs/product/knowledge-base',
+            },
+            {
+              label: 'Launch & success support',
+              to: '/docs/product/launch-success-support',
+            },
+          ],
+        },
+        {
+          title: 'Solutions',
+          items: [
+            {
+              label: 'Case studies',
+              to: '/docs/solutions/case-studies',
+            },
+            {
+              label: 'Open source',
+              to: '/docs/solutions/open-source',
+            },
+            {
+              label: 'Custom development',
+              to: '/docs/solutions/custom-development',
+            },
+          ],
+        },
+        {
+          title: 'Developers',
+          items: [
+            {
+              label: 'Getting started',
+              to: '/docs/developers/getting-started',
+            },
+            {
+              label: 'Architecture',
+              to: '/docs/developers/architecture',
+            },
+            {
+              label: 'In-production usage guide',
+              to: '/docs/developers/in-production-usage-guide',
+            },
+            {
+              label: 'API documentation',
+              to: '/docs/developers/api-documentation',
+            },
+            {
+              label: 'Plugins',
+              to: '/docs/developers/plugins',
+            },
+          ],
+        },
+        {
+          title: 'About',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'Contact us',
+              to: '/about/contact-us',
+            },
+            {
+              label: 'Careers',
+              to: '/about/careers',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/cerpus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Edlib &mdash; <a href="https://cerpus.com">Cerpus</a>`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ sourcecode/docs/docs.edlib.com/src/components/HomepageFeatures.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Edlib was designed from the ground up to be ...
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Edlib lets you focus on your ...
+      </>
+    ),
+  },
+  {
+    title: 'Powered by H5P',
+    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Edlib ...
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
