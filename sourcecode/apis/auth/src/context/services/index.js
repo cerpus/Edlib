333d0a49624179e030de4dc2c1e51d146cde5be7
Max Moeschinger <m.moeschinger@gmail.com>
2021-06-01 10:39:30 +0200

    cleanup

--- sourcecode/apis/auth/src/context/services/index.js
+++ sourcecode/apis/auth/src/context/services/index.js
@@ -1,14 +1,3 @@
-import apiConfig from '../../config/apis.js';
-import version from '@cerpus/edlib-node-utils/apiClients/version/index.js';
-import externalResourceFetcher from './externalResourceFetcher.js';
-import elasticsearch from './elasticsearch/index.js';
-
 export default (req, res) => {
-    const versionApi = version(req, apiConfig.version);
-
-    return {
-        version: versionApi,
-        externalResourceFetcher: externalResourceFetcher(req),
-        elasticsearch: elasticsearch(req),
-    };
+    return {};
 };
