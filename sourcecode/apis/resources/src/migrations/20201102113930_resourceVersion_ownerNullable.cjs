65690254aac17a64cd3ffd7e9479836746e1e7f6
Max Moeschinger <m.moeschinger@gmail.com>
2021-06-01 12:50:35 +0200

    add support sqlite3

--- sourcecode/apis/resources/src/migrations/20201102113930_resourceVersion_ownerNullable.cjs
+++ sourcecode/apis/resources/src/migrations/20201102113930_resourceVersion_ownerNullable.cjs
@@ -1,11 +1,17 @@
-exports.up = function (knex) {
+exports.up = async function (knex) {
+    if (knex.client.config.client === 'sqlite3') {
+        await knex.schema.table('resourceVersions', function (table) {
+            table.dropColumn('ownerId');
+        });
+
+        return knex.schema.table('resourceVersions', function (table) {
+            table.string('ownerId', 100).nullable();
+        });
+    }
+
     return knex.schema.alterTable('resourceVersions', function (table) {
         table.string('ownerId', 100).nullable().alter();
     });
 };
 
-exports.down = function (knex) {
-    return knex.schema.table('resourceVersions', function (table) {
-        table.string('ownerId', 100).notNullable().alter();
-    });
-};
+exports.down = async function (knex) {};
