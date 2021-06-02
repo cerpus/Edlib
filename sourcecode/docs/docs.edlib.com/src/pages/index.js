d726b3f7c46095b977e6fd9297019ebc05d35da7
Brett Kromkamp <brett.kromkamp@cerpus.com>
2021-06-02 10:07:43 +0200

    Add initial Features page documentation (for testing purposes)

--- sourcecode/docs/docs.edlib.com/src/pages/index.js
+++ sourcecode/docs/docs.edlib.com/src/pages/index.js
@@ -13,6 +13,13 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Documentation &mdash; Intro
+          </Link>
+        </div>
       </div>
     </header>
   );
