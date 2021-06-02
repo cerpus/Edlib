971316ffc124f5a61f7391ea6a25a606297ca062
Brett Kromkamp <brett.kromkamp@cerpus.com>
2021-06-02 11:38:51 +0200

    Replace front page images and other minor updates

--- sourcecode/docs/docs.edlib.com/docusaurus.config.js
+++ sourcecode/docs/docs.edlib.com/docusaurus.config.js
@@ -17,6 +17,7 @@ module.exports = {
         src: 'img/edlib-logo.png',
       },
       items: [
+        {to: '/docs/intro', label: 'Documentation', position: 'left'},
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/cerpus',
@@ -130,15 +131,9 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
