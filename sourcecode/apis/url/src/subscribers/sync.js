618e838e795d046cdb2502de668f3e93b1850e2e
Max Moeschinger <m.moeschinger@gmail.com>
2021-06-14 12:14:49 +0200

    fix sentry import

--- sourcecode/apis/url/src/subscribers/sync.js
+++ sourcecode/apis/url/src/subscribers/sync.js
@@ -1,4 +1,4 @@
-import Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
 import { buildRawContext } from '../context/index.js';
 import { logger } from '@cerpus/edlib-node-utils';
 import moment from 'moment';
