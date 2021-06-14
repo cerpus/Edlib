425b6c3bdb85d46e58c100f91080701570b2fc18
Max Moeschinger <m.moeschinger@gmail.com>
2021-06-14 12:14:28 +0200

    add new usage migration script

--- sourcecode/proxies/admin/src/routes/Jobs/routes/MigrateCore/MigrateCore.jsx
+++ sourcecode/proxies/admin/src/routes/Jobs/routes/MigrateCore/MigrateCore.jsx
@@ -8,23 +8,30 @@ const MigrateCore = () => {
             <Grid container>
                 <Grid item md={12}>
                     <Job
-                        name="Flytt url fra core til edlib2"
+                        name="1. Flytt url fra core til edlib2"
                         startUrl="/url/v1/sync-resources"
                         statusUrl={(jobId) => `/url/v1/sync-resources/${jobId}`}
                     />
                 </Grid>
                 <Grid item md={12}>
                     <Job
-                        name="Sync ressurser"
+                        name="2. Sync ressurser"
                         startUrl="/resources/v1/jobs/migrate-old-data"
                         statusUrl={(jobId) => `/resources/v1/jobs/${jobId}`}
                     />
                 </Grid>
                 <Grid item md={12}>
                     <Job
-                        name="Sync lti"
+                        name="3. Sync lti"
                         startUrl="/lti/v1/sync-lti"
                         statusUrl={(jobId) => `/lti/v1/sync-lti/${jobId}`}
+                    />
+                </Grid>
+                <Grid item md={12}>
+                    <Job
+                        name="4. Sync lti usage views with resourceapi"
+                        startUrl="/resources/v1/jobs/sync-lti-usage-views"
+                        statusUrl={(jobId) => `/resources/v1/jobs/${jobId}`}
                     />
                 </Grid>
             </Grid>
--- sourcecode/proxies/admin/src/routes/Jobs/routes/Resources.jsx
+++ sourcecode/proxies/admin/src/routes/Jobs/routes/Resources.jsx
@@ -8,6 +8,13 @@ const Resources = () => {
             <Grid container>
                 <Grid item md={12}>
                     <Job
+                        name="Sync lti usage views with resourceapi"
+                        startUrl="/resources/v1/jobs/sync-lti-usage-views"
+                        statusUrl={(jobId) => `/resources/v1/jobs/${jobId}`}
+                    />
+                </Grid>
+                <Grid item md={12}>
+                    <Job
                         name="Oppdater elasticsearch index"
                         startUrl="/resources/v1/jobs/refresh-elasticsearch-index"
                         statusUrl={(jobId) => `/resources/v1/jobs/${jobId}`}
