8f59bd35b8f39c3c792fa2478f17b981fe0d54f6
Max Moeschinger <m.moeschinger@gmail.com>
2021-07-08 13:34:17 +0200

    new endpoint to retrieve multiple resources by external references

--- sourcecode/apis/resources/src/routes/index.js
+++ sourcecode/apis/resources/src/routes/index.js
@@ -162,6 +162,10 @@ export default async ({ pubSubConnection }) => {
         '/v1/resources-from-external/:externalSystemName/:externalSystemId',
         runAsync(resourceController.getResourceFromExternalId)
     );
+    apiRouter.post(
+        '/v1/resources/by-external-references',
+        runAsync(resourceController.getResourceFromExternalReferences)
+    );
 
     /**
      * @swagger
