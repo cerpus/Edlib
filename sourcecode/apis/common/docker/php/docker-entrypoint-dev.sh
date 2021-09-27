fb2c1362903ab29821db15c1c5dbf7054c587b3b
Max Moeschinger <m.moeschinger@gmail.com>
2021-09-27 13:27:26 +0200

    move deploy to own container

--- sourcecode/apis/common/docker/php/docker-entrypoint-dev.sh
+++ sourcecode/apis/common/docker/php/docker-entrypoint-dev.sh
@@ -4,4 +4,12 @@ set -e
 composer install
 chmod -R o+w bootstrap/cache storage
 
+echo "Waiting for database to be ready..."
+
+until php artisan app:db-ready; do
+    sleep 1
+done
+
+php artisan migrate --force
+
 exec docker-entrypoint.sh "$@"
--- sourcecode/apis/common/docker/php/docker-entrypoint.sh
+++ sourcecode/apis/common/docker/php/docker-entrypoint.sh
@@ -3,12 +3,4 @@ set -e
 
 php artisan optimize
 
-echo "Waiting for database to be ready..."
-
-until php artisan app:db-ready; do
-    sleep 1
-done
-
-php artisan migrate --force
-
 exec docker-php-entrypoint "$@"
