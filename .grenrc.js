60e941eca164502e45e5e5f5f57ef958fe1b6cc9
Max Moeschinger <m.moeschinger@gmail.com>
2021-10-14 14:17:53 +0200

    Use "gren" to create good release notes

---
+++ .grenrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "prefix": "",
+    "ignoreIssuesWith": [
+        "duplicate",
+        "wontfix",
+        "invalid",
+        "help wanted"
+    ],
+    "template": {
+        "issue": "- [{{text}}]({{url}}) {{name}}"
+    },
+    "groupBy": {
+        "Enhancements:": ["enhancement"],
+        "Bug Fixes:": ["bug"]
+    }
+};
