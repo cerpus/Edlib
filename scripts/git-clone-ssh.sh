26defa66cf9162f7fc627c1a6ec754444e521552
Max Moeschinger <m.moeschinger@gmail.com>
2021-10-29 08:44:41 +0200

    Move remaining repos away from private stash

--- scripts/git-clone-ssh.sh
+++ scripts/git-clone-ssh.sh
@@ -7,12 +7,7 @@ pushd ../sourcecode/not_migrated
 
 TRANSPORT=ssh://git@app-cerpus-stash.cerpus.net:7999/
 
-git clone ${TRANSPORT}edlib/doku-lti-viewer.git
 git clone ${TRANSPORT}brain/edlibfacade.git
 git clone ${TRANSPORT}brain/licenseapi.git
-git clone ${TRANSPORT}rs/re-recommender.git
-git clone ${TRANSPORT}rs/re-content-index.git
-git clone ${TRANSPORT}brain/versionclient.git
-git clone ${TRANSPORT}brain/edlibfacade-test.git
 
 popd
