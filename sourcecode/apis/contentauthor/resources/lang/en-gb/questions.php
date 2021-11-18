482e9526abea74799764ada130abc8c8ada2ad7f
nimda <christian.einvik@cerpus.com>
2021-11-18 13:01:27 +0000

    Issue #134: Update text for QuestionSet preview

--- sourcecode/apis/contentauthor/resources/lang/en-gb/questions.php
+++ sourcecode/apis/contentauthor/resources/lang/en-gb/questions.php
@@ -7,5 +7,5 @@ return [
     "error.no-cards" => "Please add at least one question.",
     "error.question-missing-text" => "One or more questions are missing text.",
     "error.answer-missing-text" => "One or more answers is missing text.",
-    "preview" => "This is an Edlib Question set. In order to use this as a learning resource, please edit it and choose presentation form at the bottom and follow the instructions from there.",
+    "preview" => "This is an Edlib Question set and cannot be displayed in its current form. You need to edit it and choose presentation form at the bottom.",
 ];
--- sourcecode/apis/contentauthor/resources/lang/nb-no/questions.php
+++ sourcecode/apis/contentauthor/resources/lang/nb-no/questions.php
@@ -7,5 +7,5 @@ return [
     "error.no-cards" => "Du må legge til minst et spørsmål.",
     "error.question-missing-text" => "Et eller flere spørsmål mangler tekst.",
     "error.answer-missing-text" => "Et eller flere svar mangler tekst.",
-    "preview" => "Dette er et Edlib spørsmålssett. For å bruke dette som en læringsressurs, må å du redigere det og velge presentasjonsform i bunnen av det og følge instruksjonene derfra.",
+    "preview" => "Dette er et Edlib spørsmålssett og kan ikke vises i sin nåværende form. Du må redigere det og velge presentasjonsform i nederst på siden.",
 ];
