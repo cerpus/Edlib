6ca03999bc9659cf49904a77a356c7b8b0533f62
nimda <christian.einvik@cerpus.com>
2021-11-18 12:49:14 +0000

    Issue #134: Better translatable message for QuestionSet preview

--- sourcecode/apis/contentauthor/resources/lang/en-gb/questions.php
+++ sourcecode/apis/contentauthor/resources/lang/en-gb/questions.php
@@ -6,5 +6,6 @@ return [
     "error.title" => "A title is required.",
     "error.no-cards" => "Please add at least one question.",
     "error.question-missing-text" => "One or more questions are missing text.",
-    "error.answer-missing-text" => "One or more answers is missing text."
+    "error.answer-missing-text" => "One or more answers is missing text.",
+    "preview" => "This is an Edlib Question set. In order to use this as a learning resource, please edit it and choose presentation form at the bottom and follow the instructions from there.",
 ];
--- sourcecode/apis/contentauthor/resources/lang/nb-no/questions.php
+++ sourcecode/apis/contentauthor/resources/lang/nb-no/questions.php
@@ -7,4 +7,5 @@ return [
     "error.no-cards" => "Du må legge til minst et spørsmål.",
     "error.question-missing-text" => "Et eller flere spørsmål mangler tekst.",
     "error.answer-missing-text" => "Et eller flere svar mangler tekst.",
+    "preview" => "Dette er et Edlib spørsmålssett. For å bruke dette som en læringsressurs, må å du redigere det og velge presentasjonsform i bunnen av det og følge instruksjonene derfra.",
 ];
