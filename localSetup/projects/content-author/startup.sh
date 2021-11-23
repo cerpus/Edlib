f589a093c850cc5d64b4c89412cf02257820b115
Max Moeschinger <m.moeschinger@gmail.com>
2021-11-23 15:17:33 +0100

    Cleanup

--- localSetup/projects/content-author/startup.sh
+++ localSetup/projects/content-author/startup.sh
@@ -5,7 +5,8 @@ set -eux
 update-ca-certificates
 /start-scripts/wait-for-multiple.sh mysql:3306 nginx:80
 cd /app
-chown -R www-data:www-data /app/storage /app/public
+mkdir -p storage/framework/cache storage/framework/views storage/framework/sessions
 composer install
 php artisan migrate --force
+chown -R www-data:www-data /app/storage /app/public
 php-fpm -R -F -O
