d20fa4af373ee4320f99b681fe7ba5e6268d4751
Max Moeschinger <m.moeschinger@gmail.com>
2021-12-10 13:22:08 +0100

    Remove logstash logging and log in JSON format

--- sourcecode/npm/nodeUtils/src/services/logger.js
+++ sourcecode/npm/nodeUtils/src/services/logger.js
@@ -1,25 +1,14 @@
 import Winston from 'winston';
 import appConfig from '../envConfig/app.js';
-import LogstashTransport from './logstash.js';
 
 const logger = Winston.createLogger({
     level: 'debug',
     format: Winston.format.combine(
         Winston.format.errors({ stack: true }),
-        Winston.format.simple()
+        Winston.format.json()
     ),
     silent: !appConfig.enableLogging,
-    transports: [
-        new Winston.transports.Console(),
-        !appConfig.isTest &&
-            appConfig.logstashUrl &&
-            new LogstashTransport({
-                logstash: {
-                    host: appConfig.logstashUrl,
-                    port: 9605,
-                },
-            }),
-    ].filter(Boolean),
+    transports: [new Winston.transports.Console()],
 });
 
 export default logger;
