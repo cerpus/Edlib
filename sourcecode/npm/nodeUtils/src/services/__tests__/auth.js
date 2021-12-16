1f2a3f897d822cdb6155ec75f945114435549591
Max Moeschinger <max@momentech.no>
2021-12-16 09:44:20 +0100

    Improve auth error handling and add test

---
+++ sourcecode/npm/nodeUtils/src/services/__tests__/auth.js
@@ -0,0 +1,64 @@
+import { getKeyFromAuth, verifyTokenAgainstAuth } from '../auth';
+import createJWKSMock from 'mock-jwks';
+
+describe('apiClients', () => {
+    describe('edlibAuth', () => {
+        describe('verifyTokenAgainstAuth', () => {
+            test('To return payload if valid token is passed', async () => {
+                const url = new URL(
+                    'http://www.test.com/.well-known/jwks.json'
+                );
+                const jwksMock = createJWKSMock(
+                    `${url.protocol}//${url.host}`,
+                    url.pathname
+                );
+                const payload = {
+                    payload: 'smth',
+                };
+
+                jwksMock.start();
+                const token = jwksMock.token(payload);
+
+                expect(
+                    await verifyTokenAgainstAuth({}, url.toString())(token)
+                ).toStrictEqual(payload);
+
+                await jwksMock.stop();
+            });
+            test('To return null on invalid token', async () => {
+                const invalidToken = () => {
+                    const url = new URL(
+                        'http://www.test2.com/.well-known/jwks.json'
+                    );
+                    const jwksMock = createJWKSMock(
+                        `${url.protocol}//${url.host}`,
+                        url.pathname
+                    );
+
+                    return jwksMock.token({
+                        payload: 'smth',
+                    });
+                };
+
+                const url = new URL(
+                    'http://www.test.com/.well-known/jwks.json'
+                );
+                const jwksMock = createJWKSMock(
+                    `${url.protocol}//${url.host}`,
+                    url.pathname
+                );
+
+                jwksMock.start();
+
+                expect(
+                    await verifyTokenAgainstAuth(
+                        {},
+                        url.toString()
+                    )(invalidToken())
+                ).toStrictEqual(null);
+
+                await jwksMock.stop();
+            });
+        });
+    });
+});
