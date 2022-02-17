481d2311564f79bdf7d5090197bfcb99bf120ad5
admin <weblate@cerpus.com>
2022-02-17 12:47:56 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 40.0% (2 of 5 strings)
    
    Translation: Content Author/PHP - passwords
    Translate-URL: https://weblate.edlib.com/projects/content-author/php-passwords/nb_NO/

--- sourcecode/apis/contentauthor/resources/lang/nb_NO/passwords.php
+++ sourcecode/apis/contentauthor/resources/lang/nb_NO/passwords.php
@@ -1,1 +1,15 @@
 <?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Passord må ha minst seks tegn og må være lik bekreftelsen.',
+    'reset' => 'Ditt passord har blitt nullstilt!',
+];
