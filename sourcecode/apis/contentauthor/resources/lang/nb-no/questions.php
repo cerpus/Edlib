a5952bd1e9a21be4c84c27327bc4e45d90e1ed09
Tor-Martin Karlsen <tormartinkarlsen@cerpus.com>
2022-02-17 14:56:30 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: Content Author/PHP - questions
    Translate-URL: https://weblate.edlib.com/projects/content-author/php-questions/nb_NO/

--- sourcecode/apis/contentauthor/resources/lang/nb-no/questions.php
+++ sourcecode/apis/contentauthor/resources/lang/nb-no/questions.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    "questions" => "Spørsmål",
-    "create-question" => "Opprett spørsmål",
-    "edit-question" => "Rediger spørsmål",
-    "error.title" => "Gi samlingen en tittel",
-    "error.no-cards" => "Du må legge til minst et spørsmål.",
-    "error.question-missing-text" => "Et eller flere spørsmål mangler tekst.",
-    "error.answer-missing-text" => "Et eller flere svar mangler tekst.",
-    "preview" => "Dette er et Edlib spørsmålssett og kan ikke vises i sin nåværende form. Du må redigere det og velge presentasjonsform i nederst på siden.",
+    'questions' => "Spørsmål",
+    'create-question' => "Opprett spørsmål",
+    'edit-question' => "Rediger spørsmål",
+    'error.title' => "Tittel er påkrevd.",
+    'error.no-cards' => "Du må legge til minst et spørsmål.",
+    'error.question-missing-text' => "Et eller flere spørsmål mangler tekst.",
+    'error.answer-missing-text' => "Et eller flere svar mangler tekst.",
+    'preview' => "Dette er et Edlib spørsmålssett og kan ikke vises i sin nåværende form. Du må redigere det og velge presentasjonsform i nederst på siden.",
 ];
