4a7e8df7d3ba82a5ab8a63a0411f3118adc46507
max <max@momentech.no>
2022-02-18 08:15:00 +0100

    Fix some string that were not translated

--- sourcecode/npm/components/src/i18n/index.js
+++ sourcecode/npm/components/src/i18n/index.js
@@ -1,4 +1,8 @@
 import i18n from 'i18next';
+import moment from 'moment';
+
+// Also import moment locals when creating a new language
+import 'moment/locale/nb';
 
 const i = i18n.createInstance();
 
@@ -19,6 +23,10 @@ i.init({
     react: {
         useSuspense: false,
     },
+});
+
+i.on('languageChanged', (lng) => {
+    moment.locale(lng);
 });
 
 export default i;
