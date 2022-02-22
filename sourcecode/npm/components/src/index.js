9e672c2d3c71f0ddbe3f2af361b25a4822cae96f
Max Moeschinger <m.moeschinger@gmail.com>
2022-02-22 11:22:45 +0100

    Cleanup export file

--- sourcecode/npm/components/src/index.js
+++ sourcecode/npm/components/src/index.js
@@ -1,8 +1,4 @@
 export { default as EdlibModal } from './exportedComponents/EdlibModal';
 export { default as EditEdlibResourceModal } from './exportedComponents/EditEdlibResourceModal';
 export { EdlibComponentsProvider } from './contexts/EdlibComponents';
-export {
-    useEdlibResource,
-    withUseResource,
-} from './hooks/requests/useResource';
 export { default as useToken } from './hooks/useToken';
