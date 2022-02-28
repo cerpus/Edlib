456e0586a2456ea591d993e625ab81b7a86caf79
Max Moeschinger <m.moeschinger@gmail.com>
2022-02-28 14:12:28 +0100

    Fix blank screen for NDLA

--- sourcecode/www/src/routes/routes/StandaloneComponents/components/ContentExplorer/routes/EditResource.jsx
+++ sourcecode/www/src/routes/routes/StandaloneComponents/components/ContentExplorer/routes/EditResource.jsx
@@ -3,12 +3,14 @@ import { useHistory } from 'react-router-dom';
 import { useEdlibComponentsContext } from '../../../../../../contexts/EdlibComponents';
 import { useResourceCapabilities } from '../../../../../../contexts/ResourceCapabilities';
 import ResourceEditor from '../../../../../../components/ResourceEditor';
+import { useIframeStandaloneContext } from '../../../../../../contexts/IframeStandalone.jsx';
 
 const ResourceEditorRoute = ({ match }) => {
     const history = useHistory();
     const [loading, setLoading] = React.useState(false);
     const { onInsert } = useResourceCapabilities();
     const { getUserConfig } = useEdlibComponentsContext();
+    const { getPath } = useIframeStandaloneContext();
     const canReturnResources = getUserConfig('canReturnResources');
 
     return (
@@ -20,7 +22,7 @@ const ResourceEditorRoute = ({ match }) => {
                 if (canReturnResources) {
                     onInsert(resourceId, resourceVersionId);
                 } else {
-                    history.push(`/my-content?sortBy=created`);
+                    history.push(getPath(`/my-content?sortBy=created`));
                 }
             }}
             loading={loading}
