05038f10210d5774beaa21c73660076452288fe1
Max Moeschinger <m.moeschinger@gmail.com>
2022-03-01 11:03:49 +0100

    Fix tests

--- sourcecode/proxies/resource/src/routes/__tests__/licenses.js
+++ sourcecode/proxies/resource/src/routes/__tests__/licenses.js
@@ -1,27 +1,5 @@
 import request from '../../tests/request.js';
 
-jest.mock('@cerpus/edlib-node-utils', () => {
-    const actual = jest.requireActual('@cerpus/edlib-node-utils');
-    return {
-        ...actual,
-        apiClients: {
-            ...actual.apiClients,
-            license: () => ({
-                getAll: async () => [
-                    {
-                        id: 'PRIVATE',
-                        name: 'Private',
-                    },
-                    {
-                        id: 'CC0',
-                        name: 'Creative Commons',
-                    },
-                ],
-            }),
-        },
-    };
-});
-
 describe('Test endpoints', () => {
     describe('Licenses', () => {
         test('Get all unauthorized if no token', async () => {
@@ -40,7 +18,6 @@ describe('Test endpoints', () => {
             );
 
             expect(response.statusCode).toBe(200);
-            expect(response.body.length).toBe(2);
 
             response.body.forEach((license) => {
                 expect(license.id).toBeDefined();
