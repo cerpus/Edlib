356f066ab38f7a83d833a72e4f93c1de4f07ded4
Emma Cecilie Hughes <emma.hughes@cerpus.com>
2022-03-15 15:34:13 +0100

    upgrade cerpus/edlib-resource-kit-laravel

--- sourcecode/apis/contentauthor/config/edlib-resource-kit.php
+++ sourcecode/apis/contentauthor/config/edlib-resource-kit.php
@@ -2,6 +2,20 @@
 
 return [
 
+    //
+    // If false, resources will be published asynchronously over the message
+    // bus. This is fast, but you get no feedback in the event that publishing
+    // was unsuccessful.
+    //
+    // If true, resources will be synchronously published over HTTP. This is
+    // slower, but allows you to handle errors while publishing.
+    //
+    'synchronous-resource-manager' => false,
+
+    //
+    // RabbitMQ configuration
+    // (only required if 'synchronous-resource-manager' => false):
+    //
     'pub-sub' => Cerpus\PubSub\Connection\ConnectionFactory::class,
 
     'resource-serializer' => App\EdlibResource\ResourceSerializer::class,
