c393e43f16b6fb1204101c5717a68a6bd50e2e34
Emma Cecilie Hughes <emma.hughes@cerpus.com>
2022-03-23 14:53:51 +0100

    restore deletion that's out-of-scope

---
+++ sourcecode/apis/contentauthor/app/Http/Controllers/H5PAccessApi.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+
+class H5PAccessApi extends Controller
+{
+    public function getAccess($id)
+    {
+        $db = DB::connection()->getPdo();
+        $sql = "SELECT is_private FROM h5p_contents WHERE id=:id";
+        $params = [':id' => $id];
+        $stmt = $db->prepare($sql);
+        $stmt->execute($params);
+        $access = $stmt->fetchAll(\PDO::FETCH_CLASS);
+        /* Use this to only return value instead
+		$access = $stmt->fetch(\PDO::FETCH_COLUMN); */
+        return response()->json($access);
+    }
+}
