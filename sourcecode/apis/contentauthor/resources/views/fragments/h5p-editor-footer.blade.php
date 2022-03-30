7df68a69f260c1a37bf660f677504c0b6942ddb4
Emma Cecilie Hughes <emma.hughes@cerpus.com>
2022-03-30 15:36:29 +0200

    combine split views, remove unused views

--- sourcecode/apis/contentauthor/resources/views/fragments/h5p-editor-footer.blade.php
+++
@@ -1,9 +0,0 @@
-<script src="{{ mix('js/bootstrap.js') }}"></script>
-<script src="{{ mix('js/react-app.js') }}"></script>
-{!! $config !!}
-@foreach( $jsScript as $js)
-{!! HTML::script($js) !!}
-@endforeach
-@stack('js')
-</body>
-</html>
--- sourcecode/apis/contentauthor/resources/views/h5p/index.blade.php
+++ sourcecode/apis/contentauthor/resources/views/h5p/index.blade.php
@@ -1,5 +1,9 @@
-@include('fragments/header')
-
-<h2>{{ $message }}</h2>
-
-@include('fragments/footer')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    @include('fragments/header')
+    <h2>{{ $message }}</h2>
+</body>
+</html>
