ba908632945fa5022e2d54eb593ec807d49a1ba2
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2022-03-31 09:02:07 +0200

    combine split views, remove unused views (#941)
    * combine split views, remove unused views
    
    * remove reference to deleted view

--- sourcecode/apis/contentauthor/resources/views/h5p/index.blade.php
+++ sourcecode/apis/contentauthor/resources/views/h5p/index.blade.php
@@ -1,5 +1,8 @@
-@include('fragments/header')
-
-<h2>{{ $message }}</h2>
-
-@include('fragments/footer')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <h2>{{ $message }}</h2>
+</body>
+</html>
