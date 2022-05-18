930fc182258c8c653dc4021a33d2c3378d0b43f5
Emma Cecilie Hughes <emma.hughes@cerpus.com>
2022-05-18 14:17:30 +0200

    remove unused learning path leftovers

---
+++ sourcecode/apis/contentauthor/database/migrations/2022_05_18_080000_drop_learning_paths.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class DropLearningPaths extends Migration
+{
+    public function up(): void
+    {
+        Schema::dropIfExists('learning_path_steps');
+        Schema::dropIfExists('learning_paths');
+    }
+
+    public function down(): void
+    {
+    }
+}
