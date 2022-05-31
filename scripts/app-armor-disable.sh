180c3a71ec56fef71d4bdd5e7de79e14714c27fa
Emma Cecilie Hughes <emma.hughes@cerpus.com>
2022-05-31 15:42:55 +0200

    refresh the setup docs, remove old setup scripts

--- scripts/app-armor-disable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# update app-armor to make docker run
-sudo mkdir -p /etc/default/grub.d
-echo 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT apparmor=0"' | sudo tee /etc/default/grub.d/apparmor.cfg
-sudo update-grub
