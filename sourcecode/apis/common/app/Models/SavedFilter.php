26f251512fd1c86d1c896fb072cad5e8de668dfd
Emma Cecilie Hughes <emma.hughes@cerpus.com>
2022-08-04 14:02:06 +0200

    bring laravel 9 to common API, update dependencies

--- sourcecode/apis/common/app/Models/SavedFilter.php
+++ sourcecode/apis/common/app/Models/SavedFilter.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @mixin IdeHelperSavedFilter
+ */
 class SavedFilter extends Model
 {
     use HasFactory;
--- sourcecode/apis/common/app/Models/SavedFilterChoice.php
+++ sourcecode/apis/common/app/Models/SavedFilterChoice.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @mixin IdeHelperSavedFilterChoice
+ */
 class SavedFilterChoice extends Model
 {
     use HasFactory;
