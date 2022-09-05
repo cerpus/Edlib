bbd6a7b0021f257f5dcfe5caea5f273b6a7db37b
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2022-09-05 15:50:15 +0200

    remember open filter blocks (#1625)

--- sourcecode/www/src/constants/resourceFilters.js
+++ sourcecode/www/src/constants/resourceFilters.js
@@ -1,7 +1,3 @@
-const resourceFilters = {
-    SAVED_FILTERS: 'saved_filters',
-    H5P_TYPE: 'h5p_type',
-    LICENSE: 'license',
-};
-
-export default resourceFilters;
+export const SAVED_FILTERS = 'saved_filters';
+export const H5P_TYPE = 'h5p_type';
+export const LICENSE = 'license';
--- sourcecode/www/src/routes/routes/StandaloneComponents/components/ContentExplorer/ContentExplorer.jsx
+++ sourcecode/www/src/routes/routes/StandaloneComponents/components/ContentExplorer/ContentExplorer.jsx
@@ -14,6 +14,7 @@ import { ResourceCapabilitiesProvider } from '../../../../../contexts/ResourceCa
 import { useEdlibResource } from '../../../../../hooks/requests/useResource';
 import useRequestWithToken from '../../../../../hooks/useRequestWithToken';
 import contentExplorerLandingPages from '../../../../../constants/contentExplorerLandingPages';
+import { FilterContextProvider } from '../../../../../contexts/FilterContext';
 
 const getStartPage = (userConfiguredStartPage) => {
     if (
@@ -37,101 +38,103 @@ const ContentExplorer = ({ baseUrl, basePath, onAction }) => {
     const { edlibApi } = useConfigurationContext();
 
     return (
-        <ResourceCapabilitiesProvider
-            value={{
-                onInsert: async (resourceId, resourceVersionId, title) => {
-                    if (getUserConfig('returnLtiLinks')) {
-                        const info = await createResourceLink(
-                            resourceId,
-                            resourceVersionId
-                        );
-
-                        onAction('onLtiResourceSelected', info);
-                    } else {
-                        let url = new URL(
-                            'https://spec.edlib.com/resource-reference'
-                        );
-
-                        url.searchParams.append('resourceId', resourceId);
-
-                        if (resourceVersionId) {
-                            url.searchParams.append(
-                                'resourceVersionId',
+        <FilterContextProvider>
+            <ResourceCapabilitiesProvider
+                value={{
+                    onInsert: async (resourceId, resourceVersionId, title) => {
+                        if (getUserConfig('returnLtiLinks')) {
+                            const info = await createResourceLink(
+                                resourceId,
                                 resourceVersionId
                             );
-                        }
 
-                        onAction('onResourceSelected', {
-                            url,
-                            title,
-                        });
-                    }
-                },
-                onRemove: async (edlibId) => {
-                    await request(
-                        edlibApi(`/resources/v2/resources/${edlibId}`),
-                        'DELETE'
-                    );
-                },
-            }}
-        >
-            <div
-                style={{
-                    height: '100vh',
-                    position: 'relative',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    backgroundColor: 'white',
+                            onAction('onLtiResourceSelected', info);
+                        } else {
+                            let url = new URL(
+                                'https://spec.edlib.com/resource-reference'
+                            );
+
+                            url.searchParams.append('resourceId', resourceId);
+
+                            if (resourceVersionId) {
+                                url.searchParams.append(
+                                    'resourceVersionId',
+                                    resourceVersionId
+                                );
+                            }
+
+                            onAction('onResourceSelected', {
+                                url,
+                                title,
+                            });
+                        }
+                    },
+                    onRemove: async (edlibId) => {
+                        await request(
+                            edlibApi(`/resources/v2/resources/${edlibId}`),
+                            'DELETE'
+                        );
+                    },
                 }}
             >
-                <ContentExplorerHeader
-                    onClose={() => onAction('onClose')}
-                    getUrl={(path) => baseUrl + path}
-                />
                 <div
                     style={{
-                        flex: 1,
-                        minHeight: 0,
+                        height: '100vh',
+                        position: 'relative',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        backgroundColor: 'white',
                     }}
                 >
-                    {inMaintenanceMode && (
-                        <p style={{ padding: '1em' }}>⚠️ {t('maintenance')}</p>
-                    )}
-                    {!inMaintenanceMode && (
-                        <Switch>
-                            <Route
-                                path={`${basePath}/resources/new/:type`}
-                                component={NewResource}
-                            />
-                            <Route
-                                exact
-                                path={`${basePath}/resources/:edlibId`}
-                                component={EditResource}
-                            />
-                            <Route
-                                exact
-                                path={`${basePath}/resources/:edlibId/edit-done`}
-                                component={ResourceEditDone}
-                            />
-                            <Route
-                                exact
-                                path={`${basePath}/resources/:edlibId/:translateToLanguage`}
-                                component={EditResource}
-                            />
-                            <Route
-                                path={`${basePath}/my-content`}
-                                component={MyContent}
-                            />
-                            <Route
-                                path={`${basePath}/shared-content`}
-                                component={SharedContent}
-                            />
-                            <Redirect to={`${basePath}${startPage}${search}`} />
-                        </Switch>
-                    )}
+                    <ContentExplorerHeader
+                        onClose={() => onAction('onClose')}
+                        getUrl={(path) => baseUrl + path}
+                    />
+                    <div
+                        style={{
+                            flex: 1,
+                            minHeight: 0,
+                        }}
+                    >
+                        {inMaintenanceMode && (
+                            <p style={{ padding: '1em' }}>⚠️ {t('maintenance')}</p>
+                        )}
+                        {!inMaintenanceMode && (
+                            <Switch>
+                                <Route
+                                    path={`${basePath}/resources/new/:type`}
+                                    component={NewResource}
+                                />
+                                <Route
+                                    exact
+                                    path={`${basePath}/resources/:edlibId`}
+                                    component={EditResource}
+                                />
+                                <Route
+                                    exact
+                                    path={`${basePath}/resources/:edlibId/edit-done`}
+                                    component={ResourceEditDone}
+                                />
+                                <Route
+                                    exact
+                                    path={`${basePath}/resources/:edlibId/:translateToLanguage`}
+                                    component={EditResource}
+                                />
+                                <Route
+                                    path={`${basePath}/my-content`}
+                                    component={MyContent}
+                                />
+                                <Route
+                                    path={`${basePath}/shared-content`}
+                                    component={SharedContent}
+                                />
+                                <Redirect to={`${basePath}${startPage}${search}`} />
+                            </Switch>
+                        )}
+                    </div>
                 </div>
-            </div>
-        </ResourceCapabilitiesProvider>
+            </ResourceCapabilitiesProvider>
+        </FilterContextProvider>
     );
 };
 
