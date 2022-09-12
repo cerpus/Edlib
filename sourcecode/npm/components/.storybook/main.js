0b8eabbfdcfabb28523f20c31817cab8270c98bf
Christian Einvik <84850107+chrieinv@users.noreply.github.com>
2022-09-12 15:39:39 +0200

    Add Storybook addon for responsive view presets (#1654)

--- sourcecode/npm/components/.storybook/main.js
+++ sourcecode/npm/components/.storybook/main.js
@@ -3,5 +3,6 @@ module.exports = {
     addons: [
         '@storybook/addon-actions/register',
         '@storybook/addon-links/register',
+        '@storybook/addon-viewport',
     ],
 };
--- sourcecode/npm/components/.storybook/preview.js
+++ sourcecode/npm/components/.storybook/preview.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 import { MemoryRouter } from 'react-router-dom';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
+export const parameters = {
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+    },
+};
 addDecorator((story) => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
 ));
