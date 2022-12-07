2b9ed5fb805c3dcaf7df06e58d44db6f34b28781
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2022-12-07 14:32:17 +0100

    remove unnecessary MUI wrappers (#1903)

--- sourcecode/www/src/routes/routes/StandaloneComponents/routes/ViewResource/ViewResourceContainer.jsx
+++ sourcecode/www/src/routes/routes/StandaloneComponents/routes/ViewResource/ViewResourceContainer.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { EdlibComponentsProvider } from '../../../../../contexts/EdlibComponents';
-import { Box, Container } from '@mui/material';
 import ResourceView from '../../../../../components/ResourceView';
 
 const ViewResourceContainer = ({ match }) => {
     return (
         <EdlibComponentsProvider>
-            <Container disableGutters>
-                <Box padding={1}>
-                    <ResourceView resourceId={match.params.resourceId} />
-                </Box>
-            </Container>
+            <ResourceView resourceId={match.params.resourceId} />
         </EdlibComponentsProvider>
     );
 };
