3d7f62937a9a87cb7ba948a9a3de300e9a573f82
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-02-08 13:28:43 +0100

    don't wait for db in startup container

--- sourcecode/apis/common/docker/php/startup-dev.sh
+++ sourcecode/apis/common/docker/php/startup-dev.sh
@@ -3,13 +3,6 @@ set -e
 
 composer install
 chmod -R o+w bootstrap/cache storage
-
-echo "Waiting for database to be ready..."
-
-until php artisan app:db-ready; do
-    sleep 1
-done
-
 php artisan migrate --force
 
 exit 0
