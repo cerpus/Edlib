dfe3d67379145fd05b48021a8f82bfd2290263aa
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-02-24 11:17:37 +0100

    remove some unused env variables

--- sourcecode/apis/contentauthor/config/cerpus-auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'server' => env('CERPUS_AUTH_SERVER', 'https://auth.cerpus-course.com'),
-    'key' => env('CERPUS_AUTH_USER', ''),
-    'secret' => env('CERPUS_AUTH_SECRET', ''),
-];
