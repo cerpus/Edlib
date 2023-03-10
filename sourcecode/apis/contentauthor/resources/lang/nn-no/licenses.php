f08e5b2cece7bdd0c0361a1fda9253e9c60a9819
cerpusbot <99821289+cerpusbot@users.noreply.github.com>
2023-03-10 10:19:05 +0100

    Translations update from Weblate (#2175)
    * Translated using Weblate (Norwegian Nynorsk)
    
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: Content Author/PHP - licenses
    Translate-URL: https://weblate.edlib.com/projects/content-author/php-licenses/nn/
    
    * Translated using Weblate (Norwegian Nynorsk)
    
    Currently translated at 50.0% (20 of 40 strings)
    
    Translation: Content Author/PHP - h5p
    Translate-URL: https://weblate.edlib.com/projects/content-author/php-h5p/nn/
    
    ---------
    
    Co-authored-by: Tor-Martin Karlsen <tormartinkarlsen@cerpus.com>

--- sourcecode/apis/contentauthor/resources/lang/nn-no/licenses.php
+++ sourcecode/apis/contentauthor/resources/lang/nn-no/licenses.php
@@ -1,1 +1,12 @@
 <?php
+return [
+    'CC0' => 'CC0 Fristatus-erklæring',
+    'BY-NC-SA' => 'CC Namngjeving-Ikkje-kommersiell-Del på same vilkår',
+    'PRIVATE' => 'Privat',
+    'BY' => 'CC Namngjeving',
+    'BY-SA' => 'CC Namngjeving-Del på same vilkår',
+    'BY-ND' => 'CC Namngjeving-Inga omarbeiding',
+    'BY-NC' => 'CC Namngjeving-Ikkje-kommersiell',
+    'BY-NC-ND' => 'CC Namngjeving-Ikkje-kommersiell-Inga omarbeiding',
+    'PDM' => 'Public Domin-Merket',
+];
