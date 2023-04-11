0cc390f2755505292ca9f17ea14553ed2ee98f46
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-04-11 12:48:59 +0200

    fix overlapping content

--- sourcecode/hub/resources/views/components/content/grid.blade.php
+++ sourcecode/hub/resources/views/components/content/grid.blade.php
@@ -1,4 +1,4 @@
-<div class="row row-cols-4 gx-2 gy-2">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
     @foreach ($contents as $content)
         <div class="col"><x-content-card :content="$content" /></div>
     @endforeach
