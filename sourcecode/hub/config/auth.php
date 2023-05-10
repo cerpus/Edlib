52bbc7c9ed6dd06c45c8e8e2f06d220060f15514
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-05-10 11:15:26 +0200

    fix annoying indentation error

--- sourcecode/hub/config/auth.php
+++ sourcecode/hub/config/auth.php
@@ -36,10 +36,10 @@ return [
     */
 
     'guards' => [
-         'web' => [
-             'driver' => 'session',
-             'provider' => 'users',
-         ],
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
     ],
 
     /*
