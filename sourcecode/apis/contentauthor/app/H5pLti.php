f53935b31c3dd36e36583b0e032aa65dfc34506a
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-06-29 12:21:33 +0200

    Use OAuth 1 validator from edlib-resource-kit in CA (#2358)
    * use OAuth 1 validator from edlib-resource-kit
    
    * Run php-cs-fixer
    
    * fix phpstan baseline
    
    ---------
    
    Co-authored-by: emmachughes <emmachughes@users.noreply.github.com>

--- sourcecode/apis/contentauthor/app/H5pLti.php
+++ sourcecode/apis/contentauthor/app/H5pLti.php
@@ -3,37 +3,29 @@
 namespace App;
 
 use App\Http\Requests\LTIRequest;
-use Illuminate\Support\Facades\Request;
-use InvalidArgumentException;
+use Cerpus\EdlibResourceKit\Oauth1\Exception\ValidationException;
+use Cerpus\EdlibResourceKit\Oauth1\ValidatorInterface;
 
 class H5pLti
 {
-    public function __construct(
-        private readonly string $consumerKey,
-        private readonly string $consumerSecret,
-    ) {
-        if ($this->consumerKey === '') {
-            throw new InvalidArgumentException('$consumerKey cannot be an empty string');
-        }
-
-        if ($this->consumerSecret === '') {
-            throw new InvalidArgumentException('$consumerSecret cannot be an empty string');
-        }
+    public function __construct(private readonly ValidatorInterface $validator)
+    {
     }
 
-    /**
-     * @throws Oauth10\MissingSignatureException
-     * @throws Oauth10\UnsupportedSignatureException
-     */
     public function getValidatedLtiRequest(): LTIRequest|null
     {
-        // TODO: take request from parameter
-        /** @var \Illuminate\Http\Request $request */
-        $request = Request::instance();
-        $ltiRequest = LTIRequest::fromRequest($request);
-        if ($ltiRequest?->validateOauth10($this->consumerKey, $this->consumerSecret)) {
-            return $ltiRequest;
+        $ltiRequest = LTIRequest::fromRequest(request());
+
+        if (!$ltiRequest) {
+            return null;
         }
-        return null;
+
+        try {
+            $this->validator->validate($ltiRequest);
+
+            return $ltiRequest;
+        } catch (ValidationException) {
+            return null;
+        }
     }
 }
