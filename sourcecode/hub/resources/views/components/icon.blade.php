a8836f3eba8523c6baa46d6a41734a8e0e2788ea
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-07-14 10:14:08 +0200

    use icon font to avoid SVG cross-domain restriction

--- sourcecode/hub/resources/views/components/icon.blade.php
+++ sourcecode/hub/resources/views/components/icon.blade.php
@@ -1,15 +1,9 @@
-@php use Illuminate\Support\Facades\Vite; @endphp
-<svg {{ $attributes->except(['name', 'label'])->merge([
-    'class' => 'bi',
-    'width' => 16,
-    'height' => 16,
-    'fill' => 'currentColor',
+<span {{ $attributes->except(['name', 'label'])->merge([
+    'class' => 'bi bi-' . $name,
     ...(isset($label) ? [
         'aria-label' => $label,
         'role' => 'img',
     ] : [
         'aria-hidden' => 'true',
     ]),
-]) }}>
-    <use xlink:href="{{ Vite::asset('node_modules/bootstrap-icons/bootstrap-icons.svg') }}#{{ $name }}" />
-</svg>
+]) }}></span>
