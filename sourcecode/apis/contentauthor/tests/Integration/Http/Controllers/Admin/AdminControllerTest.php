b6d626089835472550dfd918bb35800302435316
Christian Einvik <christian.einvik@cerpus.com>
2023-07-18 08:42:13 +0200

    Add unittest for AdminController::viewFailedCalculations

--- sourcecode/apis/contentauthor/tests/Integration/Http/Controllers/Admin/AdminControllerTest.php
+++ sourcecode/apis/contentauthor/tests/Integration/Http/Controllers/Admin/AdminControllerTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Integration\Http\Controllers\Admin;
 
+use App\ApiModels\User;
+use App\Apis\AuthApiService;
 use App\ContentLock;
 use App\Events\ResourceSaved;
 use App\H5PContent;
 use App\H5PLibrary;
 use App\Http\Controllers\Admin\AdminController;
 use App\Libraries\H5P\H5PLibraryAdmin;
+use Faker\Factory;
 use Illuminate\Auth\GenericUser;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -133,5 +136,49 @@ class AdminControllerTest extends TestCase
     {
         yield [0];
         yield [3];
+    }
+
+    public function test_viewFailedCalculations(): void
+    {
+        $faker = Factory::create();
+        $authMock = $this->createMock(AuthApiService::class);
+        $this->instance(AuthApiService::class, $authMock);
+
+        $userId = $faker->uuid;
+        $user = new User($userId, 'Emily', 'QuackFaster', 'eq@duckburg.quack');
+
+        $authMock->expects($this->once())
+            ->method('getUser')
+            ->willReturn($user);
+
+        $library = H5PLibrary::factory()->create();
+
+        H5PContent::factory()->create([
+            'bulk_calculated' => H5PLibraryAdmin::BULK_UPDATED,
+            'user_id' => $userId,
+            'library_id' => $library->id,
+        ]);
+        H5PContent::factory()->create([
+            'bulk_calculated' => H5PLibraryAdmin::BULK_UNTOUCHED,
+            'user_id' => $userId,
+            'library_id' => $library->id,
+        ]);
+        $failedResource = H5PContent::factory()->create([
+            'bulk_calculated' => H5PLibraryAdmin::BULK_FAILED,
+            'user_id' => $userId,
+            'library_id' => $library->id,
+        ]);
+
+        $controller = app(AdminController::class);
+        $result = $controller->viewFailedCalculations();
+        $this->assertInstanceOf(View::class, $result);
+
+        $data = $result->getData();
+        $this->assertCount(1, $data['resources']);
+
+        $resource = $data['resources']->first();
+        $this->assertSame($failedResource->id, $resource->id);
+
+        $this->assertStringContainsString($user->getEmail(), $resource->ownerName);
     }
 }
