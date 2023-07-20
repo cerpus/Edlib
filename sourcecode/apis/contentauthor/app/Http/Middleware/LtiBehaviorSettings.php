edd83182fbfb2fe86136245ef694f26424982ca8
ssethupathy <113360941+ssethupathy@users.noreply.github.com>
2023-07-20 12:19:19 +0200

    PHPStan- Fixed test case failure

--- sourcecode/apis/contentauthor/app/Http/Middleware/LtiBehaviorSettings.php
+++ sourcecode/apis/contentauthor/app/Http/Middleware/LtiBehaviorSettings.php
@@ -24,14 +24,13 @@ class LtiBehaviorSettings
         if ($ltiRequest && $ltiRequest->getExtBehaviorSettings()) {
             $extBehaviorSettings = json_decode($ltiRequest->getExtBehaviorSettings(), true);
 
-            $registerSettings = null;
-            $validator = null;
-
             if ($context === 'view') {
                 $validator = Validator::make($extBehaviorSettings, BehaviorSettingsDataObject::$rules);
-                $registerSettings = function ($behaviorSettings) {
-                    Session::flash(SessionKeys::EXT_BEHAVIOR_SETTINGS, BehaviorSettingsDataObject::create($behaviorSettings));
-                };
+                if ($validator->fails()) {
+                    Log::error("Validation of ext_behavior_settings LTI param failed. Errors:", $validator->messages()->getMessages());
+                } else {
+                    Session::flash(SessionKeys::EXT_BEHAVIOR_SETTINGS, BehaviorSettingsDataObject::create($extBehaviorSettings));
+                }
             } elseif ($context === 'editor') {
                 $listEntry = $request->get('redirectToken');
                 if (empty($listEntry)) {
@@ -40,20 +39,17 @@ class LtiBehaviorSettings
                 }
 
                 $validator = Validator::make($extBehaviorSettings, EditorBehaviorSettingsDataObject::$rules);
-                $registerSettings = function ($editorBehaviorSettings) use ($listEntry) {
-                    $editorSettings = EditorBehaviorSettingsDataObject::create($editorBehaviorSettings);
-                    if (!empty($editorBehaviorSettings['behaviorSettings'])) {
-                        $behaviorSettings = BehaviorSettingsDataObject::create($editorBehaviorSettings['behaviorSettings']);
+                if ($validator->fails()) {
+                    Log::error("Validation of ext_behavior_settings LTI param failed for editor context. Errors:", $validator->messages()->getMessages());
+                } else {
+                    $editorSettings = EditorBehaviorSettingsDataObject::create($extBehaviorSettings);
+                    if (!empty($extBehaviorSettings['behaviorSettings'])) {
+                        $behaviorSettings = BehaviorSettingsDataObject::create($extBehaviorSettings['behaviorSettings']);
                         $editorSettings->setBehaviorSettings($behaviorSettings);
                         Session::flash(SessionKeys::EXT_BEHAVIOR_SETTINGS, $behaviorSettings);
                     }
                     Session::put(sprintf(SessionKeys::EXT_EDITOR_BEHAVIOR_SETTINGS, $listEntry), $editorSettings);
-                };
-            }
-            if ($validator->fails()) {
-                Log::error("Validation of ext_behavior_settings LTI param failed. Errors:", $validator->messages()->getMessages());
-            } else {
-                $registerSettings($extBehaviorSettings);
+                }
             }
         }
 
