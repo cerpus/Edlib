ceb802268cf45e5acf6ffb631d038545eb10e3ae
Stalin Sethupathy <113360941+ssethupathy@users.noreply.github.com>
2023-08-23 15:25:03 +0200

    #2393-New icons for navigations (#2439)
    * #2393-New icons for navigations
    
    * Run php-cs-fixer
    
    ---------
    
    Co-authored-by: ssethupathy <ssethupathy@users.noreply.github.com>

--- sourcecode/hub/app/Providers/RouteServiceProvider.php
+++ sourcecode/hub/app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+
 use function base_path;
 
 class RouteServiceProvider extends ServiceProvider
