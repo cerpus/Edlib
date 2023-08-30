16bdc9eedc1b73180abf4a779e3dd1035c8c7d18
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-08-30 12:32:39 +0200

    add hot module reloading for vite

--- sourcecode/hub/vite.config.js
+++ sourcecode/hub/vite.config.js
@@ -3,6 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: 80,
+        strictPort: true,
+        hmr: {
+            host: 'hub-vite-hmr.edlib.test',
+            clientPort: '443',
+            protocol: 'wss',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.scss', 'resources/js/app.js'],
