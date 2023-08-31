ca3885910ab75a57200aa46458178e0f963dc6c6
ssethupathy <113360941+ssethupathy@users.noreply.github.com>
2023-08-31 15:20:25 +0200

    #2388-redesign the page styles

--- sourcecode/hub/resources/views/content/mine.blade.php
+++ sourcecode/hub/resources/views/content/mine.blade.php
@@ -4,24 +4,13 @@
     @unless ($contents->isEmpty())
         <livewire:my-content-search :user="auth()->user()" wire:model="query"/>
     @else
-        <div class="no-content-found">
-            <h1 class="no-content-found-title">{{ trans('messages.alert-no-content-found-header') }}</h1>
-            <p class="no-content-found-description">{{ trans('messages.alert-no-content-found-description') }}</p>
+        <div class="no-content-found d-flex flex-column justify-content-center align-items-center">
+            <h1 class="no-content-found-title d-flex">{{ trans('messages.alert-no-content-found-header') }}</h1>
+            <p class="d-flex">{{ trans('messages.alert-no-content-found-description') }}</p>
 
-            <div class="d-flex justify-content-center mt-3 flex-column flex-md-row">
-                <a
-                    href="{{ route('content.index') }}"
-                    class="btn btn-primary mb-2 mb-md-0 me-md-2"
-                >
-                    {{ trans('messages.find-content') }}
-                </a>
-
-                <a
-                    href="{{ route('content.create') }}"
-                    class="btn btn-custom"
-                >
-                    {{ trans('messages.create-content') }}
-                </a>
+            <div class="d-flex gap-3 flex-column flex-md-row">
+                <a href="{{ route('content.index') }}" class="btn btn-primary" > {{ trans('messages.find-content') }} </a>
+                <a href="{{ route('content.create') }}" class="btn btn-secondary"> {{ trans('messages.create-content') }} </a>
             </div>
         </div>
     @endunless
