301ee0ac749bd68f7328f811997e23b87fc12bf9
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-09-07 10:52:46 +0200

    show debug info on content creator

--- sourcecode/hub/resources/views/content/launch-creator.blade.php
+++ sourcecode/hub/resources/views/content/launch-creator.blade.php
@@ -1,5 +1,14 @@
-<x-layout>
+<x-sidebar-layout>
     <x-slot:title>{{ sprintf('Create a thing with %s', $tool->name) }}</x-slot:title>
 
     <x-lti-launch :launch="$launch" />
-</x-layout>
+
+    <x-slot:sidebar>
+        @if (auth()->user()?->debug_mode ?? false)
+            <details>
+                <summary>Debug</summary>
+                <x-lti-debug :request="$launch->getRequest()" />
+            </details>
+        @endif
+    </x-slot:sidebar>
+</x-sidebar-layout>
