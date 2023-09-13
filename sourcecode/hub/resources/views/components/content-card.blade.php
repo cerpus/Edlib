00f655c660569400d3167878a90fed6fed727336
Christian Einvik <christian.einvik@cerpus.com>
2023-09-13 15:58:35 +0200

    Update design for content card. Adjustments for high-contrast mode. Update npm components to latest

--- sourcecode/hub/resources/views/components/content-card.blade.php
+++ sourcecode/hub/resources/views/components/content-card.blade.php
@@ -1,79 +1,92 @@
+{{-- ToDo: Remove these when actual values are available --}}
+@php($type = ['NDLA Virtual Tour (360)', 'Image Pair', 'Course Presentation', 'Audio', 'Interactive video'][mt_rand(0, 4)])
+@php($lang = ['ENG', 'NOB', 'NNO', 'SWE'][mt_rand(0, 3)])
+@php($views = [0, 7, 11, 58, 452, 9032, 69111, 912731, 5581751][mt_rand(0, 8)])
+{{-- End --}}
+
 @php($showDrafts ??= false)
 @php($version = $showDrafts ? $content->latestVersion : $content->latestPublishedVersion)
-<article class="border border-bottom-0 position-relative">
-    {{-- Top row--}}
-    <div class="d-flex gap-3 p-3">
-        <div class="">
-            <x-icon name="film" class="fs-1 text-secondary" />
-        </div>
 
-        <div class="flex-grow-1">
-            <h2 class="fs-5">
-                @unless ($version->published)
-                    <small class="badge bg-danger rounded-pill fs-6">
-                        {{ trans('messages.unpublished') }}
-                    </small>
-                @endunless
-
-                <a
-                    href="{{ route('content.preview', [$content->id]) }}"
-                    class="text-decoration-none link-body-emphasis"
-                >
-                    {{ $version->resource->title }}
-                </a>
-            </h2>
-
-            <p class="m-0 text-secondary d-flex flex-wrap column-gap-3">
-                @foreach ($content->users as $user)
-                    <span class="d-inline-block text-truncate"><x-icon name="person" />{{ $user->name }}</span>
-                @endforeach
-                <span class="d-inline-block text-truncate">
-                    {{-- TODO: localise date string --}}
-                    <x-icon name="clock" />{{ $content->updated_at->toDateString() }}
-                </span>
-            </p>
-        </div>
-
-        <div>
-{{--            <a--}}
-{{--                href="{{ route('content.edit', [$content->id]) }}"--}}
-{{--                class="btn btn-outline-dark"--}}
-{{--                title="{{ trans('messages.edit') }}"--}}
-{{--            >--}}
-{{--                <x-icon name="pencil" />--}}
-{{--            </a>--}}
-
-            @isset($lti['content_item_return_url'])
-                @php($request = $content->toItemSelectionRequest())
-                <form
-                    action="{{ $request->getUrl() }}"
-                    method="{{ $request->getMethod() }}"
-                    class="d-inline"
-                >
-                    {!! $request->toHtmlFormInputs() !!}
-                    <x-form.button class="btn-primary">Select</x-form.button>
-                </form>
-            @endisset
-
-{{--            <form action="{{ route('content.copy', [$content->id]) }}" method="POST" class="d-inline">--}}
-{{--                @csrf--}}
-{{--                <button--}}
-{{--                    class="btn btn-outline-dark d-inline"--}}
-{{--                    title="{{ trans('messages.copy') }}"--}}
-{{--                >--}}
-{{--                    <x-icon name="clipboard" />--}}
-{{--                </button>--}}
-{{--            </form>--}}
+<article class="card">
+    <div class="card-header border-bottom-0 fw-bold position-relative">
+        <a
+            href="{{ route('content.preview', [$content->id]) }}"
+            class="text-decoration-none link-body-emphasis"
+            aria-label="{{ trans('messages.preview') }}"
+        >
+            {{-- TODO: Date and time should be displayed in users timezone --}}
+            <div class="card-header-updated text-truncate d-none d-md-block fw-normal" title="{{$content->updated_at->isoFormat('LLLL')}}">
+                {{ trans('messages.edited') }}:
+                {{
+                    $content->updated_at->isToday() ? ucfirst(trans('messages.today')) . $content->updated_at->isoFormat(' LT') :
+                    ($content->updated_at->isSameAs('W', \Illuminate\Support\Carbon::now()) ? ucfirst($content->updated_at->isoFormat('dddd LT')) : $content->updated_at->isoFormat('LL'))
+                }}
+            </div>
+            <div class="text-line-clamp-2">
+                {{ $version->resource->title }}
+            </div>
+        </a>
+        <div class="badge position-absolute end-0 top-100 card-preview-badge d-none d-md-inline-block">
+            <x-icon name="eye"/>
+            <span title="{{ trans('messages.views') }}">{{ $views }}</span>
         </div>
     </div>
-
-    {{-- Bottom row --}}
-    <div class="
-        @if($showDrafts && !$version->published)
-            bg-danger
-        @else
-            bg-success
-        @endif
-        pb-1
-    " aria-hidden="true"></div>
+    <div class="card-body">
+        <div class="row card-text mb-2">
+            <div class="col-auto small">
+                {{ $type }}
+            </div>
+            <div class="col-auto badge text-bg-primary">
+                {{ $lang }}
+            </div>
+        </div>
+        <div class="card-text small">
+            @foreach ($content->users as $user)
+                {{ $user->name }}
+            @endforeach
+        </div>
+    </div>
+    <div class="card-footer d-flex align-items-center border-0">
+        <button class="btn btn-primary btn-sm me-1">
+            {{ trans('messages.use-content') }}
+        </button>
+        <button class="btn btn-secondary btn-sm d-none d-md-inline-block me-1">
+            {{ trans('messages.edit-content') }}
+        </button>
+        <div class="dropup">
+            <button
+                type="button"
+                class="btn dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                aria-label="{{ trans('messages.toggle-menu') }}"
+            >
+                <x-icon name="three-dots-vertical" />
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li>
+                    <a href="{{ route('content.preview', [$content->id]) }}" class="dropdown-item">
+                        <x-icon name="info-lg" class="me-2" />
+                        {{ trans('messages.preview') }}
+                    </a>
+                </li>
+                <li class="d-md-none">
+                    <a href="{{ route('content.edit', [$content->id]) }}" class="dropdown-item">
+                        <x-icon name="pencil" class="me-2" />
+                        {{ trans('messages.edit-content') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="dropdown-item">
+                        <x-icon name="x-lg" class="me-2 text-danger" />
+                        {{ trans('messages.delete-content') }}
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="badge position-absolute end-0 d-md-none card-preview-badge">
+            <x-icon name="eye"/>
+            <div title="{{ trans('messages.views') }}">{{ $views }}</div>
+        </div>
+    </div>
 </article>
--- sourcecode/hub/resources/views/components/content/grid.blade.php
+++ sourcecode/hub/resources/views/components/content/grid.blade.php
@@ -1,10 +1,41 @@
-<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 mb-3">
+@php($view = 'grid')
+<div class="row flex-row align-items-center">
+    <div class="col fw-bold mb-1">
+        {{ trans('messages.num-content-found', ['num' => $contents->total()]) }}
+    </div>
+    <div class="col-auto d-none d-sm-block">
+        @if ($view === 'grid')
+            <button
+                type="button"
+                class="btn p-0 fs-4"
+                title="{{ trans('messages.result-list') }}"
+                aria-description="{{ trans('messages.result-list-desc') }}"
+            >
+                <x-icon name="list" />
+            </button>
+        @else
+            <button
+                type="button"
+                class="btn p-0 fs-4"
+                title="{{ trans('messages.result-grid') }}"
+                aria-description="{{ trans('messages.result-list-desc') }}"
+            >
+                <x-icon name="grid-fill" />
+            </button>
+        @endif
+    </div>
+</div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 g-3 mb-3">
     @foreach ($contents as $content)
         <div class="col">
-            <x-content-card
-                :content="$content"
-                :show-drafts="$showDrafts ?? false"
-            />
+            @if ($view === 'grid')
+                <x-content-card
+                    :content="$content"
+                    :show-drafts="$showDrafts ?? false"
+                />
+            @else
+                {{-- ToDo: Create list view --}}
+            @endif
         </div>
     @endforeach
 </div>
