fc339ba6fdfa6e2ff0d98ecc45a68e928ce749a4
Stalin Sethupathy <113360941+ssethupathy@users.noreply.github.com>
2023-09-18 14:20:39 +0200

    #2388-Create "No content found" page (#2463)
    * #2388-Create "No content found" page
    
    * #2388-redesign the page styles
    
    * #2388-removed the title color for no content page

--- sourcecode/hub/resources/views/livewire/content/search.blade.php
+++ sourcecode/hub/resources/views/livewire/content/search.blade.php
@@ -1,5 +1,17 @@
 <div>
     <x-content.search :query="$query"/>
 
-    <x-content.grid :contents="$results"/>
+    @unless ($results->isEmpty())
+        <x-content.grid :contents="$results"/>
+    @else
+        <div class="no-content-found d-flex flex-column justify-content-center align-items-center">
+            <h1 class="text-secondary d-flex">{{ trans('messages.alert-no-search-content-found-header') }}</h1>
+            <p class="d-flex">{{ trans('messages.alert-no-search-content-found-description') }}</p>
+
+            <div class="d-flex gap-3 flex-column flex-md-row">
+                <a href="{{ route('content.index') }}" class="btn btn-primary" > {{ trans('messages.find-content') }} </a>
+                <a href="{{ route('content.create') }}" class="btn btn-secondary"> {{ trans('messages.create-content') }} </a>
+            </div>
+        </div>
+    @endunless
 </div>
