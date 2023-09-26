ec82d8c34db7adba140639cc25861dca0de66fb2
Christian Einvik <84850107+chrieinv@users.noreply.github.com>
2023-09-26 08:49:07 +0200

    Fix H5P translations not fully working (#2515)
    * Fix some of H5P translations not working
    * Remove untranslated H5P strings
    * Remove languages without any translations
    * Fix incorrect name for German language folder

---
+++ sourcecode/apis/contentauthor/resources/lang/de-de/h5p.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "Title" => "Titel",
+];
--- sourcecode/apis/contentauthor/resources/lang/sv-se/h5p.php
+++ sourcecode/apis/contentauthor/resources/lang/sv-se/h5p.php
@@ -1,44 +1,45 @@
 <?php
 
 return [
-    'title' => 'Titel',
-    'reuse' => 'Reuse',
-    'reuseContent' => 'Reuse Content',
-    'reuseDescription' => 'Reuse this content.',
-    'by' => 'by',
-    'showMore' => 'Show more',
-    'showLess' => 'Show less',
-    'subLevel' => 'Sublevel',
-    'licenseU' => 'Undisclosed',
-    'licenseCCBY' => 'Attribution',
-    'licenseCCBYSA' => 'Attribution-ShareAlike',
-    'licenseCCBYND' => 'Attribution-NoDerivs',
-    'licenseCCBYNC' => 'Attribution-NonCommercial',
-    'licenseCCBYNCSA' => 'Attribution-NonCommercial-ShareAlike',
-    'licenseCCBYNCND' => 'Attribution-NonCommercial-NoDerivs',
-    'licenseCC40' => '4.0 International',
-    'licenseCC30' => '3.0 Unported',
-    'licenseCC25' => '2.5 Generic',
-    'licenseCC20' => '2.0 Generic',
-    'licenseCC10' => '1.0 Generic',
-    'licenseGPL' => 'General Public License',
-    'licenseV3' => 'Version 3',
-    'licenseV2' => 'Version 2',
-    'licenseV1' => 'Version 1',
-    'licensePD' => 'Public Domain',
-    'licenseCC010' => 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
-    'licensePDM' => 'Public Domain Mark',
-    'licenseC' => 'Copyright',
-    'contentType' => 'Content Type',
-    'licenseExtras' => 'License Extras',
-    'changes' => 'Changelog',
-    'contentCopied' => 'Content is copied to the clipboard',
-    'connectionLost' => 'Connection lost. Results will be stored and sent when you regain connection.',
-    'connectionReestablished' => 'Connection reestablished.',
-    'resubmitScores' => 'Attempting to submit stored results.',
-    'offlineDialogHeader' => 'Your connection to the server was lost.',
-    'offlineDialogBody' => 'We were unable to send information about your completion of this task. Please check your internet connection.',
-    'offlineDialogRetryMessage' => 'Retrying in :num…',
-    'offlineDialogRetryButtonLabel' => 'Retry now',
-    'offlineSuccessfulSubmit' => 'Successfully submitted results.',
+    "Author" => "Författare",
+
+    "Cancel" => "Avbryt",
+    "Close" => "Stäng",
+    "Confirm action" => "Bekräfta åtgärd",
+    "Confirm" => "Bekräfta",
+
+    "Disable fullscreen" => "Inaktivera fullskärm",
+    "Download this content as a H5P file." => "Ladda ner det här innehållet som en H5P-fil.",
+    "Download" => "Ladda ner",
+
+    "Embed" => "Bädda in",
+
+    "Fullscreen" => "Fullskärm",
+
+    "Hide advanced" => "Dölj avancerad",
+
+    "Include this script on your website if you want dynamic sizing of the embedded content:" => "Inkludera detta skript på din webbplats om du vill ha dynamisk storlek av det inbäddade innehållet:",
+
+    "License" => "Licens",
+
+    "No copyright information available for this content." => "Ingen upphovsrättsinformation tillgänglig för detta innehåll.",
+
+    "Please confirm that you wish to proceed. This action is not reversible." => "Vänligen bekräfta att du vill fortsätta. Denna åtgärd går inte att ångra.",
+
+    "Rights of use" => "Användarrättigheter",
+
+    "Show advanced" => "Visa avancerad",
+    "Size" => "Storlek",
+    "Source" => "Källa",
+
+    "This content has changed since you last used it." => "Det här innehållet har ändrats sedan du senast använde det.",
+    "Thumbnail" => "Miniatyr",
+    "Title" => "Titel",
+
+    "View copyright information for this content." => "Visa upphovsrättsinformation för det här innehållet.",
+    "View the embed code for this content." => "Visa inbäddningskod för det här innehållet.",
+    "Visit H5P.org to check out more cool content." => "Besök H5P.org för att se mer häftigt innehåll.",
+
+    "Year" => "År",
+    "You'll be starting over." => "Du kommer att börja om.",
 ];
