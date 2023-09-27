d6969f5abf4550ef18696512d2c726af9c3bcc79
ssethupathy <113360941+ssethupathy@users.noreply.github.com>
2023-09-27 16:00:16 +0200

    Fixed the issues for dark mode option

--- sourcecode/hub/resources/views/components/layout.blade.php
+++ sourcecode/hub/resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <!-- Sidebar -->
-            @if ($title == trans('messages.my-content') || $title == trans('messages.explore'))
+            @if (\Illuminate\Support\Facades\Route::current()->getName() === 'content.mine' || \Illuminate\Support\Facades\Route::current()->getName() === 'content.index')
                 <x-sidebar />
             @endif
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
--- sourcecode/hub/resources/views/components/sidebar.blade.php
+++ sourcecode/hub/resources/views/components/sidebar.blade.php
@@ -1,6 +1,4 @@
-<section id="filterSection" class="col-md-3 col-lg-2 d-md-block bg-white sidebar d-none" role="complementary" aria-labelledby="filterSectionLabel">
+<section id="filterSection" class="col-md-3 col-lg-2 d-md-block sidebar d-none" role="complementary" aria-labelledby="filterSectionLabel">
     <h2 id="filterSectionLabel" class="visually-hidden">{{ trans('messages.filter-section') }}</h2>
-    <div class="position-sticky">
         <x-filter />
-    </div>
 </section>
