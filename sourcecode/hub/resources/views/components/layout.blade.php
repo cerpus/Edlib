d2cda8da7bf994dcbe33b78fe796b5fd0eaf70f8
Stalin Sethupathy <113360941+ssethupathy@users.noreply.github.com>
2023-10-05 09:12:19 +0200

    Filter options for mobile view (#2513)
    * Filter options for mobile view
    
    * Addressed the review comments about screen layout, CSS styles and reuse filter component
    
    * Fixed the issues for dark mode option
    
    * Addressed the review comments about classes and empty spaces and icon
    
    * Duplicated styles are removed

--- sourcecode/hub/resources/views/components/layout.blade.php
+++ sourcecode/hub/resources/views/components/layout.blade.php
@@ -2,7 +2,9 @@
     <div class="container-fluid">
         <div class="row">
             <!-- Sidebar -->
-            <x-sidebar />
+            @if (\Illuminate\Support\Facades\Route::current()->getName() === 'content.mine' || \Illuminate\Support\Facades\Route::current()->getName() === 'content.index')
+                <x-sidebar />
+            @endif
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <header>
                     <h1 class="fs-2">{{ $title }}</h1>
--- sourcecode/hub/resources/views/components/sidebar.blade.php
+++ sourcecode/hub/resources/views/components/sidebar.blade.php
@@ -1,34 +1,4 @@
-<section id="filterSection" class="col-md-3 col-lg-2 d-md-block bg-white sidebar" role="complementary" aria-labelledby="filterSectionLabel">
+<section id="filterSection" class="col-md-3 col-lg-2 d-md-block d-none" role="complementary" aria-labelledby="filterSectionLabel">
     <h2 id="filterSectionLabel" class="visually-hidden">{{ trans('messages.filter-section') }}</h2>
-    <div class="position-sticky">
-        <div class="accordion p-3 m-0 border-0 bd-example m-0 border-0" id="filterAccordion">
-            @foreach (['language', 'media', 'visuals', 'task', 'quizzes'] as $filterKey)
-                @php
-                    $filterLabel = trans("messages.$filterKey");
-                @endphp
-
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button id="collapse{{ ucfirst($filterKey) }}" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $filterKey }}Filter" aria-expanded="false" aria-controls="collapse{{ $filterKey }}Filter">
-                            {{ $filterLabel }}
-                        </button>
-                    </h2>
-                    <div id="collapse{{ $filterKey }}Filter" class="accordion-collapse collapse" data-bs-parent="#filterAccordion">
-                        <div class="accordion-body p-0">
-                            <ul class="list-group">
-                                @for ($i = 1; $i <= 3; $i++)
-                                    <li class="list-group-item border-0">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input me-1" type="checkbox" value="">
-                                            {{ $filterLabel }} {{ $i }}
-                                        </label>
-                                    </li>
-                                @endfor
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
+        <x-filter />
 </section>
