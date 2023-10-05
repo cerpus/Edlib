cbe82b8cbf74526d19d155c5c2d1d43ae5dfaf7c
Christian Einvik <christian.einvik@cerpus.com>
2023-10-05 13:40:32 +0200

    GH #2201: Don't use model in migration

--- sourcecode/apis/contentauthor/database/migrations/2023_10_04_143512_fix_double_encoding_of_library_metadata.php
+++ sourcecode/apis/contentauthor/database/migrations/2023_10_04_143512_fix_double_encoding_of_library_metadata.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-use App\H5PLibrary;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Collection;
 
 return new class () extends Migration {
     /**
@@ -14,17 +13,22 @@ return new class () extends Migration {
      */
     public function up()
     {
-        H5PLibrary::whereNotNull('metadata_settings')
+        DB::table('h5p_libraries')
+            ->select(['id', 'metadata_settings'])
+            ->whereNotNull('metadata_settings')
             ->orderBy('id')
             ->chunk(50, function (Collection $libraries) {
-                $libraries->each(function (H5PLibrary $library) {
+                $libraries->each(function ($library) {
                     try {
                         $decoded = json_decode($library->metadata_settings, flags:JSON_THROW_ON_ERROR);
                         if (is_string($decoded)) {
                             $data = json_decode($decoded, flags: JSON_THROW_ON_ERROR);
                             if (is_object($data)) {
-                                $library->metadata_settings = $decoded;
-                                $library->save();
+                                DB::table('h5p_libraries')
+                                    ->where('id', $library->id)
+                                    ->update([
+                                        'metadata_settings' => $decoded,
+                                    ]);
                             }
                         }
                     } catch (JsonException) {
