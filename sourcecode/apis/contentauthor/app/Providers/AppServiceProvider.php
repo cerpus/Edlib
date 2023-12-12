5254384db9b4b67b4543bf61ddef4aa1ab8ee261
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-12-12 08:58:15 +0100

    let ide-helper be registered via auto-discovery

--- sourcecode/apis/contentauthor/app/Providers/AppServiceProvider.php
+++ sourcecode/apis/contentauthor/app/Providers/AppServiceProvider.php
@@ -38,10 +38,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
-
         $this->app->singleton(CredentialStoreInterface::class, fn () => new Credentials(
             config('app.consumer-key'),
             config('app.consumer-secret'),
