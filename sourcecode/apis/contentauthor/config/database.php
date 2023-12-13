569b159de6f1f8d00af59bb9eee2f5537a83bb8d
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2023-12-13 15:02:37 +0100

    fix config mistake

--- sourcecode/apis/contentauthor/config/database.php
+++ sourcecode/apis/contentauthor/config/database.php
@@ -57,7 +57,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', env('EDLIBCOMMON_DB_HOST', '127.0.0.1')),
             'database' => env('DB_DATABASE', env('EDLIBCOMMON_DB_PREFIX', '') . 'contentauthor'),
-            'username' => env('DB_USER', env('EDLIBCOMMON_DB_USER', 'forge')),
+            'username' => env('DB_USERNAME', env('EDLIBCOMMON_DB_USER', 'forge')),
             'password' => env('DB_PASSWORD', env('EDLIBCOMMON_DB_PASSWORD', '')),
             'port' => env('DB_PORT', env('EDLIBCOMMON_DB_PORT', '3306')),
             'charset' => 'utf8mb4',
