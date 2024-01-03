fe7b98ea449d954623dbe5962c9857030160847f
Tor-Martin Karlsen <tormartinkarlsen@cerpus.com>
2024-01-03 15:27:37 +0100

    Update welcome page
    More relevant info about the application.

--- sourcecode/hub/resources/views/welcome.blade.php
+++ sourcecode/hub/resources/views/welcome.blade.php
@@ -1,5 +1,19 @@
 <x-layout>
-    <x-slot:title>Hubhubhubhubbhbubbubub</x-slot:title>
+    <x-slot:title>This is Edlib</x-slot:title>
 
-    <p>TODO: write the rest of the application</p>
+    <p>- the lightweight application that provides the heavy duty content management
+</p>
+    <p> Some of the core features:
+<ul>
+    <li>search</li>
+    <li>filtering</li>
+    <li>sharing</li>
+    <li>C.R.U.D. features (Create, Read, Updated, and Delete)</li>
+    <li>H5P support</li>
+    <li>admin inferface</li>
+    <li>LMS and CMS integration</li>
+</ul>
+
+<a href="https://docs.edlib.com">Read more about the application and the open source project.</a>
+</p>
 </x-layout>
