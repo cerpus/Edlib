426246d69491403c439eb8f89f7acaca5456e0a8
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-01-03 12:31:58 +0100

    add missing partition flag

--- sourcecode/hub/app/Http/Controllers/CookieController.php
+++ sourcecode/hub/app/Http/Controllers/CookieController.php
@@ -6,20 +6,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Middleware\EnsureFrameCookies;
 use Illuminate\Http\Response;
-use Symfony\Component\HttpFoundation\Cookie;
 
 final readonly class CookieController
 {
     public function popup(): Response
     {
-        $cookie = (new Cookie(EnsureFrameCookies::COOKIE_NAME))
-            ->withValue('1')
-            ->withSameSite(Cookie::SAMESITE_NONE)
-            ->withSecure()
-            ->withPartitioned();
-
         return response()
             ->view('cookie.popup')
-            ->withCookie($cookie);
+            ->withCookie(EnsureFrameCookies::createCookie());
     }
 }
