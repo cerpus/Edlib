c0de06718a1cc5db9ac9146fb8df4bc906051964
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-01-05 11:32:24 +0100

    remove need for stub files

--- sourcecode/hub/tests/TestCase.php
+++ sourcecode/hub/tests/TestCase.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\PendingCommand;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Substitute for {@link TestCase::artisan()} that always returns a pending
+     * command.
+     * @param array<string, string> $parameters
+     */
+    protected function command(string $command, array $parameters = []): PendingCommand
+    {
+        return new PendingCommand($this, $this->app, $command, $parameters);
+    }
 }
