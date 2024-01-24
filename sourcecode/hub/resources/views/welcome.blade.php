61fbfbb8e95c098c27fffb24feca423ec8146ac4
Tor-Martin Karlsen <tormartinkarlsen@cerpus.com>
2024-01-24 14:10:35 +0100

    Update to landing page
    Suggestion for text update for landing page

--- sourcecode/hub/resources/views/welcome.blade.php
+++ sourcecode/hub/resources/views/welcome.blade.php
@@ -3,17 +3,14 @@
 
     <p>- the lightweight application that provides the heavy duty content management
 </p>
-    <p> Some of the core features:
+    <p>Features:
 <ul>
+    <li>Create, Read, Updated, and Delete content</li>
     <li>search</li>
     <li>filtering</li>
     <li>sharing</li>
-    <li>C.R.U.D. features (Create, Read, Updated, and Delete)</li>
-    <li>H5P support</li>
     <li>admin inferface</li>
     <li>LMS and CMS integration</li>
 </ul>
-
-<a href="https://docs.edlib.com">Read more about the application and the open source project.</a>
 </p>
 </x-layout>
