edb672dc39e0ee0e18b97e1116acb0ed966d77e9
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-02-15 15:09:31 +0100

    split up content.details view

--- sourcecode/hub/resources/views/content/details.blade.php
+++ sourcecode/hub/resources/views/content/details.blade.php
@@ -75,101 +75,12 @@
 
         <x-slot:sidebar>
             @can('edit', $content)
-                <section>
-                    <h2 class="fs-5">{{ trans('messages.version-history') }}</h2>
-
-                    <ul class="list-unstyled d-flex flex-column gap-2 version-history">
-                        @foreach ($content->versions as $v)
-                            @php
-                                $isLatest = $content->latestVersion->is($v);
-                                $isCurrent = $version->is($v);
-                            @endphp
-
-                            <li
-                                @class([
-                                    'position-relative',
-                                    'text-body',
-                                    'p-3',
-                                    'border',
-                                    'rounded',
-                                    'd-flex',
-                                    'border-success-subtle' => $v->published,
-                                    'published' => $v->published,
-                                    'draft' => !$v->published,
-                                    'bg-success-subtle' => $content->latestPublishedVersion?->is($v),
-                                ])
-                            >
-                        <span class="flex-grow-1">
-                            <a
-                                href="{{ route('content.version-details', [$content, $v]) }}"
-                                @class([
-                                    'd-block',
-                                    'text-body',
-                                    'link-underline-opacity-0',
-                                    'link-underline-opacity-100-hover',
-                                    'stretched-link',
-                                    'link-underline-success' => $v->published,
-                                    'link-underline' => !$v->published,
-                                    'fw-bold' => $isCurrent,
-                                ])
-                            >
-                                <time datetime="{{ $v->created_at->format('c') }}">{{ $v->created_at }}</time>
-                            </a>
-
-                            @if ($content->latestPublishedVersion?->is($v))
-                                <small class="d-block">{{ trans('messages.current-published-version') }}</small>
-                            @elseif ($content->latestDraftVersion?->is($v))
-                                <small class="d-block">{{ trans('messages.latest-draft') }}</small>
-                            @endif
-                        </span>
-
-                                @if ($v->published)
-                                    <x-icon name="check-circle" label="{{ trans('messages.published') }}" class="text-success" />
-                                @else
-                                    <x-icon name="pencil" label="{{ trans('messages.draft') }}" class="text-body-secondary" />
-                                @endif
-                            </li>
-                        @endforeach
-                    </ul>
-                </section>
+                <x-content.details.version-history :$version />
             @endif
 
             @if (auth()->user()?->debug_mode ?? app()->hasDebugModeEnabled())
-                @php($version = $content->latestVersion)
-
-                <details>
-                    <summary class="fs-5">LTI params</summary>
-
-                    <dl>
-                        <dt>ID</dt>
-                        <dd><kbd>{{ $content->id }}</kbd></dd>
-
-                        <dt>Version ID</Dt>
-                        <dd><kbd>{{ $version->id }}</kbd></dd>
-
-                        <dt>Tool ID</dt>
-                        <dd><kbd>{{ $version->lti_tool_id }}</kbd></dd>
-
-                        <dt>Presentation launch URL</dt>
-                        <dd><kbd>{{ $version->lti_launch_url }}</kbd></dd>
-                    </dl>
-
-                    <x-lti-debug
-                        :url="$launch->getRequest()->getUrl()"
-                        :parameters="$launch->getRequest()->toArray()"
-                    />
-                </details>
-
-                <details>
-                    <summary class="fs-5">Messages</summary>
-
-                    <pre
-                        id="messages"
-                        class="debug-messages border font-monospace overflow-scroll"
-                        aria-label="messages"
-                        readonly
-                    ></pre>
-                </details>
+                <x-content.details.lti-params :$launch :$version />
+                <x-content.details.messages id="messages" />
             @endif
         </x-slot:sidebar>
     </x-layout.sidebar-layout>
