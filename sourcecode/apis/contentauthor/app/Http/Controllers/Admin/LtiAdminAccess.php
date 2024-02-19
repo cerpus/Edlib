3d2f6b3f2d52d35e2b9b2bd3c04d5ea58c2a786d
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-02-19 15:04:32 +0100

    allow admin access via LTI

---
+++ sourcecode/apis/contentauthor/app/Http/Controllers/Admin/LtiAdminAccess.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin;
+
+use App\Lti\Lti;
+use Illuminate\Auth\GenericUser;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use function abort;
+use function redirect;
+
+final readonly class LtiAdminAccess
+{
+    public function __construct(private Lti $lti)
+    {
+    }
+
+    public function __invoke(Request $request): RedirectResponse
+    {
+        $ltiRequest = $this->lti->getRequest($request);
+
+        if (!$ltiRequest?->isAdministrator()) {
+            abort(403, 'Missing Administrator role in LTI launch');
+        }
+
+        Auth::guard('sso')->login(new GenericUser([
+            'name' => $ltiRequest->getUserFullName(),
+            'roles' => ['superadmin'],
+        ]));
+
+        return redirect()->route('admin');
+    }
+}
