a96253c6a84c75fac0ea284881fda767d6d855b5
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-05-28 13:57:14 +0200

    remove node-utils (#278)

--- sourcecode/npm/nodeUtils/src/services/index.js
+++
@@ -1,9 +0,0 @@
-export { default as validateJoi } from './validateJoi.js';
-export * as cookie from './cookie.js';
-export * as jwt from './jwt.js';
-export { default as runAsync } from './runAsync.js';
-export { default as exceptionTranslator } from './exceptionTranslator.js';
-export { default as env } from './env.js';
-export * as pubsub from './pubSub.js';
-export { default as status } from './status.js';
-export * as redisHelpers from './redis.js';
