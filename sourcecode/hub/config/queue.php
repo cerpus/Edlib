b643654edf9fa1aa36e193f83845bec6cbf24535
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-06-05 11:11:59 +0200

    fix storing failed jobs

--- sourcecode/hub/config/queue.php
+++ sourcecode/hub/config/queue.php
@@ -88,7 +88,7 @@ return [
 
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'mysql'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'failed_jobs',
     ],
 
