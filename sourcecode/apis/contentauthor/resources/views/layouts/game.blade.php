516b85e0220e43862c3718ccd0491972485ba109
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-06-13 13:38:10 +0200

    add noindex, indexifembedded to CA (#2737)

--- sourcecode/apis/contentauthor/resources/views/layouts/game.blade.php
+++ sourcecode/apis/contentauthor/resources/views/layouts/game.blade.php
@@ -5,6 +5,7 @@
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="robots" content="noindex,indexifembedded">
     <link rel="stylesheet" type="text/css" href="{{mix('css/game.css')}}">
     <base href="@yield('basePath', "/")">
 @stack('css')
