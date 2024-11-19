1d46e45f9c410551668fd02e15aaa85b07e88b65
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-11-19 15:47:52 +0100

    ndla: exclude resize script

--- sourcecode/hub/resources/views/ndla-legacy/oembed.blade.php
+++ sourcecode/hub/resources/views/ndla-legacy/oembed.blade.php
@@ -1,19 +1,3 @@
 <div>
 <iframe src="{{ $src }}" title="{{ $title }}" width="800" height="600" allowfullscreen id="edlib-{{ $id }}"></iframe>
-<script>
-    addEventListener('message', (event) => {
-        const frame = document.getElementById('edlib-{{ $id }}');
-        if (event.source !== frame.contentWindow) {
-            return;
-        }
-        if (event?.data?.action !== 'resize') {
-            return;
-        }
-        frame.height = String(
-            event.data.scrollHeight +
-            frame.getBoundingClientRect().height -
-            frame.scrollHeight,
-        );
-    });
-</script>
 </div>
