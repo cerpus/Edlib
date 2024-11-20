388808baaa3649d6167ebea26135b86858165aea
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-11-20 15:30:17 +0100

    ndla: remove unnecessary div and id attribute in oembed

--- sourcecode/hub/resources/views/ndla-legacy/oembed.blade.php
+++ sourcecode/hub/resources/views/ndla-legacy/oembed.blade.php
@@ -1,3 +1,1 @@
-<div>
-<iframe src="{{ $src }}" title="{{ $title }}" width="800" height="600" allowfullscreen id="edlib-{{ $id }}"></iframe>
-</div>
+<iframe src="{{ $src }}" title="{{ $title }}" width="800" height="600" allowfullscreen></iframe>
