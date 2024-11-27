fb03924a8a9f277fa655d9c36f943c1e3d218985
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2024-11-27 13:24:51 +0100

    only forward resize messages in embed view (#2807)

--- sourcecode/hub/resources/views/content/embed.blade.php
+++ sourcecode/hub/resources/views/content/embed.blade.php
@@ -2,5 +2,5 @@
 <x-layout no-nav no-header no-footer expand>
     <x-slot:title>{{ $version->title }}</x-slot:title>
 
-    <x-lti-launch :launch="$launch" class="w-100 h-100" />
+    <x-lti-launch :launch="$launch" class="w-100 h-100" forwards-resize-messages />
 </x-layout>
