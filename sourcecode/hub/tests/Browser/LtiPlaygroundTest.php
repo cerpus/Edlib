c8f4e553fa8bd6e1a37455d69e8d31bf2cb7c7bd
emmachughes <emmachughes@users.noreply.github.com>
2024-12-12 12:37:20 +0000

    Run php-cs-fixer

--- sourcecode/hub/tests/Browser/LtiPlaygroundTest.php
+++ sourcecode/hub/tests/Browser/LtiPlaygroundTest.php
@@ -15,19 +15,20 @@ final class LtiPlaygroundTest extends DuskTestCase
     {
         $user = User::factory()->name('Dee Bugg')->create();
 
-        $this->browse(fn (Browser $browser) => $browser
-            ->loginAs($user->email)
-            ->assertAuthenticated()
-            ->visit('/')
-            ->press('Dee Bugg')
-            ->assertDontSeeLink('LTI playground')
-            ->clickLink('Preferences')
-            ->check('debug_mode')
-            ->press('Save')
-            ->press('Dee Bugg')
-            ->assertSeeLink('LTI playground')
-            ->clickLink('LTI playground')
-            ->assertTitleContains('LTI playground')
+        $this->browse(
+            fn (Browser $browser) => $browser
+                ->loginAs($user->email)
+                ->assertAuthenticated()
+                ->visit('/')
+                ->press('Dee Bugg')
+                ->assertDontSeeLink('LTI playground')
+                ->clickLink('Preferences')
+                ->check('debug_mode')
+                ->press('Save')
+                ->press('Dee Bugg')
+                ->assertSeeLink('LTI playground')
+                ->clickLink('LTI playground')
+                ->assertTitleContains('LTI playground')
         );
     }
 
@@ -36,17 +37,20 @@ final class LtiPlaygroundTest extends DuskTestCase
         $platform = LtiPlatform::factory()->create();
         $user = User::factory()->name('El Tee-Aye')->create();
 
-        $this->browse(fn (Browser $browser) => $browser
-            ->loginAs($user->email)
-            ->assertAuthenticated()
-            ->visit('/lti/playground')
-            ->type('launch_url', 'https://hub-test.edlib.test/lti/samples/presentation')
-            ->type('key', $platform->key)
-            ->type('secret', $platform->secret)
-            ->press('Launch')
-            ->withinFrame('iframe', fn (Browser $frame) => $frame
-                ->assertSee('If you can see this, the LTI launch was successful')
-            )
+        $this->browse(
+            fn (Browser $browser) => $browser
+                ->loginAs($user->email)
+                ->assertAuthenticated()
+                ->visit('/lti/playground')
+                ->type('launch_url', 'https://hub-test.edlib.test/lti/samples/presentation')
+                ->type('key', $platform->key)
+                ->type('secret', $platform->secret)
+                ->press('Launch')
+                ->withinFrame(
+                    'iframe',
+                    fn (Browser $frame) => $frame
+                        ->assertSee('If you can see this, the LTI launch was successful')
+                )
         );
     }
 }
