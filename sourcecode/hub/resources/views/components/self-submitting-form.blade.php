8114fce7f4dda31264a9c5c5e66672a22ed0a09b
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2025-01-20 14:45:58 +0100

    fix query params in self-submitting form with GET method

--- sourcecode/hub/resources/views/components/self-submitting-form.blade.php
+++ sourcecode/hub/resources/views/components/self-submitting-form.blade.php
@@ -3,13 +3,26 @@
     use Illuminate\Support\Facades\Vite;
 
     $uniqueId = Str::uuid()->toString();
+    $method ??= 'GET';
+    $target ??= '_self';
+
+    // <form> action cannot have query params if the method is GET
+    // These have to be sent as hidden inputs.
+    if (strtoupper($method) === 'GET') {
+        $urlParts = explode('?', $url, 2);
+        $url = $urlParts[0];
+        if (isset($urlParts[1])) {
+            parse_str($urlParts[1], $urlParams);
+            $parameters = array_replace($parameters ?? [], $urlParams);
+        }
+    }
 @endphp
 
 <form
     action="{{ $url }}"
-    method="{{ $method ?? 'GET' }}"
+    method="{{ $method }}"
     name="launch-form-{{ $uniqueId }}"
-    target="{{ $target ?? '_self' }}"
+    target="{{ $target }}"
 >
     @foreach($parameters as $name => $value)
         <input type="hidden" name="{{ $name }}" value="{{ $value }}">
