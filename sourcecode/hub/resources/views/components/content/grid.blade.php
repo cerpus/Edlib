605c5cd679c21d835affc2c75c8c8869222ff2a7
Christian Einvik <christian.einvik@cerpus.com>
2025-02-06 14:03:59 +0100

    Undo removal of fixed cards for different widths

--- sourcecode/hub/resources/views/components/content/grid.blade.php
+++ sourcecode/hub/resources/views/components/content/grid.blade.php
@@ -1,6 +1,6 @@
 @props(['contents'])
 
-<div class="row g-3">
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 g-3">
     @foreach ($contents as $content)
         <div class="col">
             <x-content.card :$content />
