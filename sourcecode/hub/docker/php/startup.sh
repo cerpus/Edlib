6ea970773a00d0c32f94199047a3c0ef97165f48
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2025-02-14 11:52:54 +0100

    improve docker setup for devs

--- sourcecode/hub/docker/php/startup.sh
+++ sourcecode/hub/docker/php/startup.sh
@@ -2,5 +2,5 @@
 set -ex
 
 php artisan migrate --force
-php artisan scout:index 'App\Models\Content'
-php artisan scout:sync-index-settings
+php artisan scout:index 'App\Models\Content' --quiet
+php artisan scout:sync-index-settings --quiet
