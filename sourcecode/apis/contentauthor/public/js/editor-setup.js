41cba802d3a2c3078853ac42e37874c750aa9669
Christian Einvik <84850107+chrieinv@users.noreply.github.com>
2025-03-20 10:35:32 +0100

    GH #3051: Fix fail to upgrade content to newer H5P content type version in Editor (#3052)

--- sourcecode/apis/contentauthor/public/js/editor-setup.js
+++ sourcecode/apis/contentauthor/public/js/editor-setup.js
@@ -4,31 +4,15 @@
         if (Integration === undefined) {
             return;
         }
-        if (typeof CKEDITOR !== 'undefined') {
-            if (typeof Integration.editor.wirisPath !== 'undefined') {
-                // Add wiris plugin
-                CKEDITOR.plugins.addExternal('ckeditor_wiris', Integration.editor.wirisPath);
-            }
-
-            if (Integration.hasOwnProperty('editor') && Integration.editor.hasOwnProperty('extraAllowedContent')) {
-                if( CKEDITOR.hasOwnProperty('extraAllowedContent') !== true){
-                    CKEDITOR.config.extraAllowedContent = "";
-                }
-                CKEDITOR.config.extraAllowedContent += Integration.editor.extraAllowedContent;
-            }
-
-            if( Integration.hasOwnProperty('editor') && Integration.editor.hasOwnProperty('editorBodyClass') ){
-                CKEDITOR.config.bodyClass = Integration.editor.editorBodyClass;
-            }
-        }
-
-        if (window.parent !== null && typeof H5PEditor !== "undefined") {
-            window.parent.IframeH5PEditor = H5PEditor;
-        }
 
         if (Integration.hasOwnProperty('crossorigin') && Integration.crossorigin === true &&
             Integration.hasOwnProperty('crossoriginRegex') && Integration.crossoriginRegex !== null) {
             Integration.crossoriginRegex = new RegExp(Integration.crossoriginRegex);
+        }
+
+        // Used by 'Update content' functionality, upgrading to a newer version of the content type, in Editor
+        if (window.parent !== null && typeof H5PEditor !== "undefined") {
+            window.parent.IframeH5PEditor = H5PEditor;
         }
     });
 })(typeof H5P !== "undefined" ? H5P.jQuery : $);
