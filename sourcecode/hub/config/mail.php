79e219757b0483658440c6bd63ac76db9abb8a20
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2025-03-28 13:01:52 +0100

    add SES config

--- sourcecode/hub/config/mail.php
+++ sourcecode/hub/config/mail.php
@@ -53,6 +53,9 @@ return [
 
         'ses' => [
             'transport' => 'ses',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'eu-west-1'),
         ],
 
         'mailgun' => [
