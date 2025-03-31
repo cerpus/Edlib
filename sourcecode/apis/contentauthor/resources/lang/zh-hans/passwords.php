151cc466f8a96505da423e4ed00e867c8ce5a17c
Sam Xu <sam.z.xu@gmail.com>
2025-03-31 20:30:10 +0800

    Add Simplified Chinese language files (zh-hans)

---
+++ sourcecode/apis/contentauthor/resources/lang/zh-hans/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少包含六个字符，并且与确认密码一致。',
+    'reset' => '您的密码已重置！',
+    'sent' => '我们已经通过电子邮件发送了密码重置链接！',
+    'token' => '此密码重置令牌无效。',
+    'user' => "我们找不到使用该电子邮件地址的用户。",
+
+];
