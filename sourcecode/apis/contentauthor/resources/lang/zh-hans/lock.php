28c6ad0a39e5311261704383b3e01a0a804d5fe4
Emma C. Hughes <84008144+emmachughes@users.noreply.github.com>
2025-04-03 13:23:49 +0200

    update available locales, fix syntax errors

--- sourcecode/apis/contentauthor/resources/lang/zh-hans/lock.php
+++ sourcecode/apis/contentauthor/resources/lang/zh-hans/lock.php
@@ -3,11 +3,11 @@
 return [
     'locked-title' => "内容已锁定",
     'article-is-locked' => "内容已被锁定，无法编辑。",
-    'article-will-expire' => '锁定将在<span id="expires">:expires</span>分钟后过期<b>或</b>当:editor保存时。",
-    'edit' => '编辑",
-    'edit-most-recent' => '编辑最新版本",
-    'open-for-edit' => '内容已开放编辑。",
-    'no-new-version' => '未创建新版本",
+    'article-will-expire' => '锁定将在<span id="expires">:expires</span>分钟后过期<b>或</b>当:editor保存时。',
+    'edit' => '编辑',
+    'edit-most-recent' => '编辑最新版本',
+    'open-for-edit' => '内容已开放编辑。',
+    'no-new-version' => '未创建新版本',
     'new-version-created' => "此内容的新版本已存在。",
-    'view' => '查看",
+    'view' => '查看',
 ];
