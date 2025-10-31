04124d619bdae0bdf80a53c0f7aaeac2f91a07ba
Christian Einvik <84850107+chrieinv@users.noreply.github.com>
2025-10-31 11:43:00 +0100

    GH #3162: Use PSK from config not env (#3201)

--- sourcecode/apis/contentauthor/routes/rest.php
+++ sourcecode/apis/contentauthor/routes/rest.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\API\H5PLibraryController;
 
 // Get the H5P library (content type) title for library machine-names
 Route::post('v1/h5p/library/title', [H5PLibraryController::class, 'getLibraryTitleByMachineName'])
-    ->middleware(['auth.psk:X-PSK,LTI_CONSUMER_KEY']);
+    ->middleware(['auth.psk:X-PSK,app.consumer-key']);
