afd398ce487773393e0e7361009563273d8ed34a
Max Moeschinger <maxmoeschinger@users.noreply.github.com>
2026-02-17 11:18:31 +0100

    Improve performance of leaf query

---
+++ sourcecode/hub/database/migrations/2026_02_17_101729_add_indexes_for_leaf_versions_query.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('content_version_tag', function (Blueprint $table) {
+            $table->index('tag_id');
+            $table->index('content_version_id');
+        });
+
+        Schema::table('content_versions', function (Blueprint $table) {
+            $table->index('previous_version_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('content_version_tag', function (Blueprint $table) {
+            $table->dropIndex(['tag_id']);
+            $table->dropIndex(['content_version_id']);
+        });
+
+        Schema::table('content_versions', function (Blueprint $table) {
+            $table->dropIndex(['previous_version_id']);
+        });
+    }
+};
