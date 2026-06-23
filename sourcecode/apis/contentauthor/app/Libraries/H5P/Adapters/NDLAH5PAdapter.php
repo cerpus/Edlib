8d201d8a045b0c01fe6e8062c4d845478a2cfbe8
edlib-oddarne <odd-arne@edlib.com>
2026-06-23 08:17:09 +0200

    Fix: rendering stops

--- sourcecode/apis/contentauthor/app/Libraries/H5P/Adapters/NDLAH5PAdapter.php
+++ sourcecode/apis/contentauthor/app/Libraries/H5P/Adapters/NDLAH5PAdapter.php
@@ -289,7 +289,6 @@ class NDLAH5PAdapter implements H5PAdapterInterface
 
     public function showCustomCssForNewContentTypes(?H5PContent $h5pContent = null): bool
     {
-        return false;
         if (is_null($h5pContent)) {
             return false;
         }
