0af038021af7e17b6b7ba6a3eeb6c011bb3e4878
Gunnar Velle <gunnar.velle@gmail.com>
2026-08-19 08:44:50 +0200

    Use db-connection setup for failed jobs config.

--- sourcecode/apis/contentauthor/config/queue.php
+++ sourcecode/apis/contentauthor/config/queue.php
@@ -94,7 +94,7 @@ return [
     */
 
     'failed' => [
-        'database' => 'mysql', 'table' => 'failed_jobs',
+        'database' => env('DB_CONNECTION', 'mysql'), 'table' => 'failed_jobs',
     ],
 
 ];
